1c3b6b6c4babdc065211a783fa257676c9121e07
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-01-17 10:05:30 +0100

    Exception: Add Show-FGTException for display Exception info when use Invoke-RestMethod (or -WebRequest)

---
+++ PowerFGT/Private/Exception.ps1
@@ -0,0 +1,32 @@
+
+#
+# Copyright 2018, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Show-FGTException() {
+    Param(
+        [parameter(Mandatory = $true)]
+        $Exception
+    )
+
+    If ($Exception.Exception.Response) {
+        $result = $Exception.Exception.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($result)
+        $responseBody = $reader.ReadToEnd()
+
+        $responseJson =  $responseBody | ConvertFrom-Json
+
+        Write-Warning "The FortiGate API sends an error message:"
+        Write-Warning "Error description (code): $($Exception.Exception.Response.StatusDescription) ($($Exception.Exception.Response.StatusCode.Value__))"
+        if($responseBody) {
+            if($responseJson.message) {
+                Write-Warning "Error details: $($responseJson.message)"
+            } else {
+                Write-Warning "Error details: $($responseBody)"
+            }
+        } elseif($Exception.ErrorDetails.Message) {
+            Write-Warning "Error details: $($Exception.ErrorDetails.Message)"
+        }
+    }
+}
