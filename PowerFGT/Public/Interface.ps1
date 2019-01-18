5750afff305356a47e29d17e6a5294f982d70c08
Benjamin PERRIER <ben.perrier@outlook.fr>
2019-01-18 15:50:42 +0100

    Add Get-FGTInterface to diplay interfaces

---
+++ PowerFGT/Public/Interface.ps1
@@ -0,0 +1,18 @@
+﻿function Get-FGTInterface {
+
+    Param(
+        [Parameter(Mandatory = $false)]
+        [switch]$SimpleView 
+         )
+    
+    if ($SimpleView -eq $False) {
+            $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/system/interface' -method 'GET'
+            $reponse.results
+    } else {
+    
+            $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/system/interface' -method 'GET'
+            $reponse.results | select status, name, alias, role, ip, mode, type, allowaccess, macaddr | ft
+    }
+
+    
+}
