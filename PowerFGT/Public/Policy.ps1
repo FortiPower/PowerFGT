332528851b03d56d411cbf163f36853a3cded69d
Benjamin PERRIER <ben.perrier@outlook.fr>
2019-01-18 16:14:30 +0100

    Add Get-FGTPolicy to display policies

---
+++ PowerFGT/Public/Policy.ps1
@@ -0,0 +1,20 @@
+﻿function Get-FGTPolicy {
+    
+    Param(
+        [Parameter(Mandatory = $false)]
+        [switch]$SimpleView 
+         )
+    
+    if ($SimpleView -eq $False) {
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall/policy' -method 'GET'
+        $reponse.results
+    } else { 
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall/policy' -method 'GET'
+        $reponse.results | ft
+    }
+
+
+
+
+
+    }
