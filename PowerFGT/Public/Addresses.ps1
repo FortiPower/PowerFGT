a40f6f4a7bd2e195b9bae8d0790ef572f9be7149
Benjamin PERRIER <ben.perrier@outlook.fr>
2019-01-18 15:42:10 +0100

    Add Get-FGTAddresses to diplay addresses object

---
+++ PowerFGT/Public/Addresses.ps1
@@ -0,0 +1,20 @@
+﻿function Get-FGTAddresses {
+    
+    Param(
+        [Parameter(Mandatory = $false)]
+        [switch]$SimpleView 
+         )
+    
+    if ($SimpleView -eq $False) {
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall/address' -method 'GET'
+        $reponse.results
+    } else { 
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall/address' -method 'GET'
+        $reponse.results | ft
+    }
+
+
+
+
+
+    }
