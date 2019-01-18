5f7c839b4a45039a49d7230ea4b1d7f56a1c69a4
Benjamin PERRIER <ben.perrier@outlook.fr>
2019-01-18 16:37:24 +0100

    Add Get-FGTDns to display dns infos

---
+++ PowerFGT/Public/Dns.ps1
@@ -0,0 +1,19 @@
+﻿
+
+function Get-FGTDns {
+    
+    Param(
+        [Parameter(Mandatory = $false)]
+        [switch]$SimpleView 
+         )
+    
+    if ($SimpleView -eq $False) {
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/system/dns' -method 'GET'
+        $reponse.results
+    } else { 
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/system/dns' -method 'GET'
+        $reponse.results | ft
+    }
+
+
+    }
