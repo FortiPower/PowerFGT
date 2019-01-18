7407fb935c2a342fb5d7ae0d7338bbdb6bb6079b
Benjamin PERRIER <ben.perrier@outlook.fr>
2019-01-18 16:34:53 +0100

    Add Get-FGTStaticRoute to display Static Routes

---
+++ PowerFGT/Public/StaticRoute.ps1
@@ -0,0 +1,19 @@
+﻿
+
+function Get-FGTStaticRoute {
+    
+    Param(
+        [Parameter(Mandatory = $false)]
+        [switch]$SimpleView 
+         )
+    
+    if ($SimpleView -eq $False) {
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/router/static' -method 'GET'
+        $reponse.results
+    } else { 
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/router/static' -method 'GET'
+        $reponse.results | ft
+    }
+
+
+    }
