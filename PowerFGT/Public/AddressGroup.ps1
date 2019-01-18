39701c686e52af57d864f4179c64d9fbd6eeae5c
Benjamin PERRIER <ben.perrier@outlook.fr>
2019-01-18 16:20:31 +0100

    Add Get-FGTAddrgrp to display Address Group

---
+++ PowerFGT/Public/AddressGroup.ps1
@@ -0,0 +1,19 @@
+﻿
+
+function Get-FGTAddrgrp {
+    
+    Param(
+        [Parameter(Mandatory = $false)]
+        [switch]$SimpleView 
+         )
+    
+    if ($SimpleView -eq $False) {
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall/addrgrp' -method 'GET'
+        $reponse.results
+    } else { 
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall/addrgrp' -method 'GET'
+        $reponse.results | ft
+    }
+
+
+    }
