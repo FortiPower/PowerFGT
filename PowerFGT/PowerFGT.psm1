3719c8b2acf45dde336ba27736f2e732e568025d
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-01-21 16:01:08 +0100

    PowerFGT(.psm1): Add cmdb and monitor (Public) Folder
    for match with API (cmdb is for get and configure) and monitor for get statics/status

--- PowerFGT/PowerFGT.psm1
+++ PowerFGT/PowerFGT.psm1
@@ -1,9 +1,11 @@
 #Get public and private function definition files.
 $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Public_cmdb  = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\*.ps1 -ErrorAction SilentlyContinue )
+$Public_monitor  = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 #Dot source the files
-Foreach($import in @($Public + $Private))
+Foreach($import in @($Public + + $Public_cmdb + $Public_monitor + $Private))
 {
     Try
     {
