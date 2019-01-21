e32ce8c7f05a48dcfc094749717c7feda2c80ba2
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-01-21 16:36:27 +0100

    Address: Add copyright

--- PowerFGT/Public/cmdb/address.ps1
+++ PowerFGT/Public/cmdb/address.ps1
@@ -1,4 +1,9 @@
-﻿
+﻿#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+# Copyright 2019, Benjamin Perrier <ben dot perrier at outlook dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 function Get-FGTAddresses {
 
     Param(
