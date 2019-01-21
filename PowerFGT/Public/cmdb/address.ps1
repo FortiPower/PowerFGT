efc3783680868c76d8f1c1447efcacd9d3ac4f36
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-01-21 20:31:33 +0100

    address: Remove SimpleView parameter (not need)

--- PowerFGT/Public/cmdb/address.ps1
+++ PowerFGT/Public/cmdb/address.ps1
@@ -6,17 +6,8 @@
 #
 function Get-FGTAddress {
 
-    Param(
-        [Parameter(Mandatory = $false)]
-        [switch]$SimpleView
-         )
 
-    if ($SimpleView -eq $False) {
-        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall/address' -method 'GET'
-        $reponse.results
-    } else {
-        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall/address' -method 'GET'
-        $reponse.results | ft
-    }
+    $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall/address' -method 'GET'
+    $reponse.results
 
-    }
+}
