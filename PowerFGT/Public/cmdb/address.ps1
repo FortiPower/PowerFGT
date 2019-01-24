f2acc7927ef343e1848c21275aee92798a0f0e39
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-01-24 06:15:45 +0100

    address(.ps1): add Add-FGTAddress
    Only support add ipmask type for the moment...

--- PowerFGT/Public/cmdb/address.ps1
+++ PowerFGT/Public/cmdb/address.ps1
@@ -4,6 +4,60 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+function Add-FGTAddress {
+
+    <#
+        .SYNOPSIS
+        Add a FortiGate Address
+
+        .DESCRIPTION
+        Add a FortiGate Address (ipmask, fqdn, widlcard...)
+
+        .EXAMPLE
+        Add-FGTAddress -type ipmask -Name FGT -ip 192.2.0.0 -mask 255.255.255.0
+
+        Add Address objet type ipmask with name FGT and value 192.2.0.0/24
+
+    #>
+
+    Param(
+        [Parameter (Mandatory=$true)]
+        [string]$type,
+        [Parameter (Mandatory=$true)]
+        [string]$name,
+        [Parameter (Mandatory=$false)]
+        [ipaddress]$ip,
+        [Parameter (Mandatory=$false)]
+        [ipaddress]$mask
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $uri = "api/v2/cmdb/firewall/address"
+
+        $address = new-Object -TypeName PSObject
+
+        $address | add-member -name "type" -membertype NoteProperty -Value $type
+
+        $address | add-member -name "name" -membertype NoteProperty -Value $name
+
+        $subnet = $ip.ToString()
+        $subnet += "/"
+        $subnet += $mask.ToString()
+        $address | add-member -name "subnet" -membertype NoteProperty -Value $subnet
+
+        Invoke-FGTRestMethod -method "POST" -body $address -uri $uri | out-Null
+
+    }
+
+    End {
+    }
+}
+
+
 function Get-FGTAddress {
 
     <#
