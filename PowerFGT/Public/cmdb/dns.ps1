583291f5d55ab867ccd74638c6d7ff24dc9c22d7
Benjamin <ben.perrier@outlook.fr>
2019-02-15 11:15:13 +0100

    add dns function

---
+++ PowerFGT/Public/cmdb/dns.ps1
@@ -0,0 +1,25 @@
+﻿#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+# Copyright 2019, Benjamin Perrier <ben dot perrier at outlook dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Get-FGTDns {
+  
+       <#
+        .SYNOPSIS
+        Get dns addresses configured.
+        .DESCRIPTION
+        Show dns addresses configured.
+        .EXAMPLE
+        Get-FGTDns
+        Display all dns addresses.
+        
+    #>  
+
+
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/system/dns' -method 'GET'
+        $reponse.results
+
+    }
