9d290c1dfcbd1c34c1595caba3a4f86a7a0c5ca6
Benjamin <ben.perrier@outlook.fr>
2019-02-15 11:17:04 +0100

    add addressgroup function

---
+++ PowerFGT/Public/cmdb/addressgroup.ps1
@@ -0,0 +1,23 @@
+﻿#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+# Copyright 2019, Benjamin Perrier <ben dot perrier at outlook dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Get-FGTAddressGroup {
+
+     <#
+        .SYNOPSIS
+        Get addresses group configured.
+        .DESCRIPTION
+        Show addresses group configured.
+        .EXAMPLE
+        Get-FGTAddrgrp
+        Display all addresses group.
+    #>
+    
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall/addrgrp' -method 'GET'
+        $reponse.results
+
+    }
