3552238fe98331300af96c685c0af676c0569181
Benjamin <ben.perrier@outlook.fr>
2019-02-15 10:36:32 +0100

    add staticroute function

---
+++ PowerFGT/Public/cmdb/staticroute.ps1
@@ -0,0 +1,28 @@
+﻿#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+# Copyright 2019, Benjamin Perrier <ben dot perrier at outlook dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Get-FGTStaticRoute{
+
+  <#
+      .SYNOPSIS
+      Get list of all "static routes"
+
+      .DESCRIPTION
+      Get list of all "static routes"
+
+      .EXAMPLE
+      Get-FGTStaticRoute
+
+      Get list of all static route object
+  #>
+
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/router/static' -method 'GET'
+        $reponse.results
+
+
+
+}
