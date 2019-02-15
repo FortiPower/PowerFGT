ad0833c02f3a4c6e2c6310e1ae10dac0ffb2167c
Benjamin <ben.perrier@outlook.fr>
2019-02-15 11:50:20 +0100

    add servicegroup function

---
+++ PowerFGT/Public/cmdb/servicegroup.ps1
@@ -0,0 +1,26 @@
+﻿#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+# Copyright 2019, Benjamin Perrier <ben dot perrier at outlook dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Get-FGTServiceGroup {
+
+  <#
+      .SYNOPSIS
+      Get list of all "services group"
+
+      .DESCRIPTION
+      Get list of all "services group" 
+
+      .EXAMPLE
+      Get-FGTService
+
+      Get list of all services group object
+  #>
+
+
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall.service/group' -method 'GET'
+        $reponse.results
+
+        }
