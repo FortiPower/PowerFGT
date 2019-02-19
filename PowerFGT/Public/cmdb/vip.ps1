9c512a4cd470bec6dcb5c7ae011e17311af6650b
Benjamin <ben.perrier@outlook.fr>
2019-02-19 10:53:45 +0100

    add vip function

---
+++ PowerFGT/Public/cmdb/vip.ps1
@@ -0,0 +1,25 @@
+﻿#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+# Copyright 2019, Benjamin Perrier <ben dot perrier at outlook dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Get-FGTNatVip {
+
+  <#
+      .SYNOPSIS
+      Get list of all "nat vip"
+
+      .DESCRIPTION
+      Get list of all "nat vip" 
+
+      .EXAMPLE
+      Get-FGTNatVip
+
+      Get list of all nat vip object
+  #>
+
+$reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall/vip' -method 'GET'
+$reponse.results
+
+        }
