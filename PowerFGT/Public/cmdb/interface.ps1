d7281234500db64cecb823a4684db6afd611c1c2
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-03-03 16:04:28 +0100

    interface: fix indent/typo and enhance .DESCRIPTION

--- PowerFGT/Public/cmdb/interface.ps1
+++ PowerFGT/Public/cmdb/interface.ps1
@@ -1,22 +1,26 @@
-﻿
-
+﻿#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+# Copyright 2019, Benjamin Perrier <ben dot perrier at outlook dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 function Get-FGTInterface {
 
-  <#
+    <#
       .SYNOPSIS
-      Get list of all interfaces
+      Get list of all interface
 
       .DESCRIPTION
-      Get list of all interface
+      Get list of all interface (name, IP Address, description, mode ...)
 
       .EXAMPLE
       Get-FGTInterface
 
       Get list of all interface
 
-  #>
+    #>
 
 
-        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/system/interface' -method 'GET'
-        $reponse.results
-   }
+    $response = Invoke-FGTRestMethod -uri 'api/v2/cmdb/system/interface' -method 'GET'
+    $response.results
+}
