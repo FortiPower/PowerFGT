8bce59b4a4bf9feb884d6b8330e5c77219246d9a
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-03-03 17:11:38 +0100

    vip: Fix indent/typo and enhance .DESCRIPTION

--- PowerFGT/Public/cmdb/vip.ps1
+++ PowerFGT/Public/cmdb/vip.ps1
@@ -4,22 +4,22 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-function Get-FGTNatVip {
+function Get-FGTVip {
 
-  <#
+    <#
       .SYNOPSIS
-      Get list of all "nat vip"
+      Get list of all (NAT) Virtual IP
 
       .DESCRIPTION
-      Get list of all "nat vip" 
+      Get list of all (NAT) Virtual IP (Ext IP, mapped IP, type...)
 
       .EXAMPLE
-      Get-FGTNatVip
+      Get-FGTVip
 
       Get list of all nat vip object
-  #>
+    #>
 
-$reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall/vip' -method 'GET'
-$reponse.results
+    $response = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall/vip' -method 'GET'
+    $response.results
 
-        }
+}
