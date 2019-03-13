70f45533d6e21b08560a65d820b17d9b54e55c73
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-03-13 13:44:02 +0100

    vpnipsec: Add Get-FGTVpnIPsecPhase2 cmdlet to get phase 2 settings

--- PowerFGT/Public/cmdb/vpnipsec.ps1
+++ PowerFGT/Public/cmdb/vpnipsec.ps1
@@ -23,3 +23,26 @@ function Get-FGTVpnIpsecPhase1 {
     $response = Invoke-FGTRestMethod -uri 'api/v2/cmdb/vpn.ipsec/phase1' -method 'GET'
     $response.results
 }
+
+
+
+function Get-FGTVpnIpsecPhase2 {
+
+  <#
+    .SYNOPSIS
+    Get list of all VPN IPsec phase 2 (IKE) settings
+
+    .DESCRIPTION
+    Get list of all VPN IPsec phase 2 (Local / Remote Networkn PFS, Cipher, Hash...)
+
+    .EXAMPLE
+    Get-FGTVpnIPsecPhase2
+
+    Get list of all settings of VPN IPsec Phase 2
+
+  #>
+
+
+  $response = Invoke-FGTRestMethod -uri 'api/v2/cmdb/vpn.ipsec/phase2' -method 'GET'
+  $response.results
+}
