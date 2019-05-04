0157d18b7630fa785e86bb4c7293f3ab1b6817e6
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-05-04 11:57:41 +0200

    HA: Add Get (System) HA cmdlet

---
+++ PowerFGT/Public/cmdb/ha.ps1
@@ -0,0 +1,44 @@
+﻿#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Get-FGTHA {
+
+    <#
+        .SYNOPSIS
+        Get list of HA Settings
+
+        .DESCRIPTION
+        Get list of HA Settings (Mode, Group-ID, Group-Name, Password, monitor...)
+
+        .EXAMPLE
+        Get-FGTHA
+
+        Get HA Settings
+
+    #>
+
+    Param(
+        [Parameter(Mandatory = $false)]
+        [switch]$skip
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/system/ha' -method 'GET' @invokeParams
+        $reponse.results
+    }
+
+    End {
+    }
+}
