6e6a2f207f40895fd40151c8ceb446d863a1e5c5
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-05-04 11:58:05 +0200

    Global(System): Add Get (System) Global cmdlet

---
+++ PowerFGT/Public/cmdb/systemglobal.ps1
@@ -0,0 +1,44 @@
+﻿#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Get-FGTSystemGlobal {
+
+    <#
+        .SYNOPSIS
+        Get list of System Global Settings
+
+        .DESCRIPTION
+        Get list of System Global Settings (hostname, alias....)
+
+        .EXAMPLE
+        Get-FGTSystemGlobal
+
+        Get list of all System Global Settings
+
+    #>
+
+    Param(
+        [Parameter(Mandatory = $false)]
+        [switch]$skip
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/system/global' -method 'GET' @invokeParams
+        $reponse.results
+    }
+
+    End {
+    }
+}
