8420d4be12bd788b8bd8131359e96305ac4a4777
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-05-05 19:11:18 +0200

    cmdb: move ipsec (vpn) cmdlet to ipsec (vpn) folder
    Split on 2 file phase1interface and phase2interface

--- PowerFGT/PowerFGT.psm1
+++ PowerFGT/PowerFGT.psm1
@@ -6,11 +6,12 @@ $Public_cmdb_firewall_service = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb
 $Public_cmdb_system = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\system\*.ps1 -ErrorAction SilentlyContinue )
 $Public_cmdb_user = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\user\*.ps1 -ErrorAction SilentlyContinue )
 $Public_cmdb_router = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\router\*.ps1 -ErrorAction SilentlyContinue )
+$Public_cmdb_vpnipsec = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\vpn\ipsec\*.ps1 -ErrorAction SilentlyContinue )
 $Public_monitor = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 #Dot source the files
-Foreach ($import in @($Public + $Public_cmdb + $Public_cmdb_firewall + $Public_cmdb_firewall_service + $Public_cmdb_system + $Public_cmdb_user + $Public_cmdb_router + $Public_monitor + $Private)) {
+Foreach ($import in @($Public + $Public_cmdb + $Public_cmdb_firewall + $Public_cmdb_firewall_service + $Public_cmdb_system + $Public_cmdb_user + $Public_cmdb_router + $Public_cmdb_vpnipsec + $Public_monitor + $Private)) {
     Try {
         . $import.fullname
     }
---
+++ PowerFGT/Public/cmdb/vpn/ipsec/phase2-interface.ps1
@@ -0,0 +1,43 @@
+﻿#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+function Get-FGTVpnIpsecPhase2Interface {
+
+    <#
+        .SYNOPSIS
+        Get list of all VPN IPsec phase 2 (IKE) settings
+
+        .DESCRIPTION
+        Get list of all VPN IPsec phase 2 (Local / Remote Network PFS, Cipher, Hash...)
+
+        .EXAMPLE
+        Get-FGTVpnIPsecPhase2Interface
+
+        Get list of all settings of VPN IPsec Phase 2 interface
+
+        .EXAMPLE
+        Get-FGTVpnIPsecPhase2Interface -skip
+
+        Get list of all settings of VPN IPsec Phase 2 interface (but only relevant attributes)
+    #>
+
+    Param(
+        [Parameter(Mandatory = $false)]
+        [switch]$skip
+    )
+
+    Begin {
+    }
+
+    Process {
+        $response = Invoke-FGTRestMethod -uri 'api/v2/cmdb/vpn.ipsec/phase2-interface' -method 'GET'
+        $response.results
+    }
+
+    End {
+    }
+}
