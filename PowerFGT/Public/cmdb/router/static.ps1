72c91646ad789d375201ecd9b6d9ab2318e5cb33
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-05-09 18:36:57 +0200

    cmdb(Router): Add support of Multi Connection (using -connection)

--- PowerFGT/Public/cmdb/router/static.ps1
+++ PowerFGT/Public/cmdb/router/static.ps1
@@ -34,7 +34,9 @@ function Get-FGTRouterStatic {
         [Parameter(Mandatory = $false)]
         [switch]$skip,
         [Parameter(Mandatory = $false)]
-        [String[]]$vdom
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection=$DefaultFGTConnection
     )
 
     Begin {
@@ -50,7 +52,7 @@ function Get-FGTRouterStatic {
             $invokeParams.add( 'vdom', $vdom )
         }
 
-        $response = Invoke-FGTRestMethod -uri 'api/v2/cmdb/router/static' -method 'GET' @invokeParams
+        $response = Invoke-FGTRestMethod -uri 'api/v2/cmdb/router/static' -method 'GET' -connection $connection @invokeParams
         $response.results
     }
 
