7de6c60fd447c32d61f9b761776b00602f1f0b9a
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-05-09 20:42:29 +0200

    VIP: fix typo on cmdlet name (missing l)

--- PowerFGT/Public/cmdb/firewall/vip.ps1
+++ PowerFGT/Public/cmdb/firewall/vip.ps1
@@ -4,7 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-function Get-FGTFirewalVip {
+function Get-FGTFirewallVip {
 
     <#
         .SYNOPSIS
@@ -14,12 +14,12 @@ function Get-FGTFirewalVip {
         Get list of all (NAT) Virtual IP (Ext IP, mapped IP, type...)
 
         .EXAMPLE
-        Get-FGTFirewalVip
+        Get-FGTFirewallVip
 
         Get list of all nat vip object
 
         .EXAMPLE
-        Get-FGTFirewalVip -skip
+        Get-FGTFirewallVip -skip
 
         Get list of all nat vip object (but only relevant attributes)
     #>
