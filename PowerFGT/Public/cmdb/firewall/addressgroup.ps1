8e23832b97a560da134b3a6eaa844b98a8b793da
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-11-26 09:33:39 +0100

    AddressGroup: Add Add-FGTFirewallAddressGroup
    Add Address Group, it is possible to add multiple member
    Don't check if the member is present or not...

--- PowerFGT/Public/cmdb/firewall/addressgroup.ps1
+++ PowerFGT/Public/cmdb/firewall/addressgroup.ps1
@@ -4,7 +4,93 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+function Add-FGTFirewallAddressGroup {
 
+    <#
+        .SYNOPSIS
+        Add a FortiGate Address Group
+
+        .DESCRIPTION
+        Add a FortiGate Address Group
+
+        .EXAMPLE
+        Add-FGTFirewallAddressGroup -name MyAddressGroup -member MyAddress1
+
+        Add Address Group with member MyAddress1
+
+        .EXAMPLE
+        Add-FGTFirewallAddressGroup -name MyAddressGroup -member MyAddress1, MyAddress2
+
+        Add Address Group with members MyAddress1 and MyAddress2
+
+        .EXAMPLE
+        Add-FGTFirewallAddressGroup -name MyAddressGroup -member MyAddress1 -comment "My Address Group"
+
+        Add Address Group with member MyAddress1 and a comment
+    #>
+
+    Param(
+        [Parameter (Mandatory = $true)]
+        [string]$name,
+        [Parameter (Mandatory = $true)]
+        [string[]]$member,
+        [Parameter (Mandatory = $false)]
+        [ValidateLength(0, 255)]
+        [string]$comment,
+        [Parameter (Mandatory = $false)]
+        [boolean]$visibility,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        $uri = "api/v2/cmdb/firewall/addrgrp"
+
+        $addrgrp = new-Object -TypeName PSObject
+
+        $addrgrp | add-member -name "name" -membertype NoteProperty -Value $name
+
+        #Add member to Member Array
+        $members = @( )
+        foreach ( $m in $member ) {
+            $member_name = @{ }
+            $member_name.add( 'name', $m)
+            $members += $member_name
+        }
+        $addrgrp | add-member -name "member" -membertype NoteProperty -Value $members
+
+        if ( $PsBoundParameters.ContainsKey('comment') ) {
+            $addrgrp | add-member -name "comment" -membertype NoteProperty -Value $comment
+        }
+
+        if ( $PsBoundParameters.ContainsKey('visibility') ) {
+            if ( $visibility ) {
+                $addrgrp | add-member -name "visibility" -membertype NoteProperty -Value "enable"
+            }
+            else {
+                $addrgrp | add-member -name "visibility" -membertype NoteProperty -Value "disable"
+            }
+        }
+
+        Invoke-FGTRestMethod -method "POST" -body $addrgrp -uri $uri -connection $connection @invokeParams #| out-Null
+
+        Get-FGTFirewallAddressGroup -connection $connection @invokeParams | Where-Object { $_.name -eq $name }
+    }
+
+    End {
+    }
+}
 function Get-FGTFirewallAddressgroup {
 
     <#
