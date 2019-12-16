a4a4f83a5063c8c4a2ca38b20798346b9487fe97
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-12-16 22:49:11 +0100

    group(firewallService): Add Multi Connection Support

--- PowerFGT/Public/cmdb/firewall/service/group.ps1
+++ PowerFGT/Public/cmdb/firewall/service/group.ps1
@@ -33,7 +33,9 @@ function Get-FGTFirewallServiceGroup {
         [Parameter(Mandatory = $false)]
         [switch]$skip,
         [Parameter(Mandatory = $false)]
-        [String[]]$vdom
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
     )
 
     Begin {
@@ -49,7 +51,7 @@ function Get-FGTFirewallServiceGroup {
             $invokeParams.add( 'vdom', $vdom )
         }
 
-        $response = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall.service/group' -method 'GET' @invokeParams
+        $response = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall.service/group' -method 'GET' -connection $connection @invokeParams
         $response.results
     }
 
