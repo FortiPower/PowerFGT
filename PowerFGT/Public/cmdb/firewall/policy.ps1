ab03e784985dfc61ab380a9f69059fa3109e5103
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-12-17 06:49:19 +0100

    policy(firewall): Add new filtering You can also filter by policyid !

--- PowerFGT/Public/cmdb/firewall/policy.ps1
+++ PowerFGT/Public/cmdb/firewall/policy.ps1
@@ -20,6 +20,26 @@ function Get-FGTFirewallPolicy {
         Get list of all policies
 
         .EXAMPLE
+        Get-FGTFirewallPolicy -name myPolicy
+
+        Get Policy named myPolicy
+
+        .EXAMPLE
+        Get-FGTFirewallPolicy -policyid 23
+
+        Get policy with id 23
+
+        .EXAMPLE
+        Get-FGTFirewallPolicy -name FGT -filter_type contains
+
+        Get policy contains with *FGT*
+
+        .EXAMPLE
+        Get-FGTFirewallPolicy -uuid 9e73a10e-1772-51ea-a8d7-297686fd7702
+
+        Get policy with uuid 9e73a10e-1772-51ea-a8d7-297686fd7702
+
+        .EXAMPLE
         Get-FGTFirewallPolicy -skip
 
         Get list of all policies (but only relevant attributes)
@@ -30,7 +50,26 @@ function Get-FGTFirewallPolicy {
         Get list of all policies on vdomX
     #>
 
+    [CmdletBinding(DefaultParameterSetName = "default")]
     Param(
+        [Parameter (Mandatory = $false, Position = 1, ParameterSetName = "name")]
+        [string]$name,
+        [Parameter (Mandatory = $false, ParameterSetName = "uuid")]
+        [string]$uuid,
+        [Parameter (Mandatory = $false, ParameterSetName = "policyid")]
+        [string]$policyid,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [string]$filter_attribute,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "name")]
+        [Parameter (ParameterSetName = "uuid")]
+        [Parameter (ParameterSetName = "filter")]
+        [ValidateSet('equal', 'contains')]
+        [string]$filter_type = "equal",
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [psobject]$filter_value,
         [Parameter(Mandatory = $false)]
         [switch]$skip,
         [Parameter(Mandatory = $false)]
@@ -50,6 +89,30 @@ function Get-FGTFirewallPolicy {
         }
         if ( $PsBoundParameters.ContainsKey('vdom') ) {
             $invokeParams.add( 'vdom', $vdom )
+        }
+
+        #Filtering
+        switch ( $PSCmdlet.ParameterSetName ) {
+            "name" {
+                $filter_value = $name
+                $filter_attribute = "name"
+            }
+            "uuid" {
+                $filter_value = $uuid
+                $filter_attribute = "uuid"
+            }
+            "policyid" {
+                $filter_value = $policyid
+                $filter_attribute = "policyid"
+            }
+            default { }
+        }
+
+        #if filter value and filter_attribut, add filter (by default filter_type is equal)
+        if ( $filter_value -and $filter_attribute ) {
+            $invokeParams.add( 'filter_value', $filter_value )
+            $invokeParams.add( 'filter_attribute', $filter_attribute )
+            $invokeParams.add( 'filter_type', $filter_type )
         }
 
         $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/firewall/policy' -method 'GET' -connection $connection @invokeParams
