c4c0090def5630fe40d8ffe84a83bb03e8115615
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-12-21 08:09:42 +0100

    Policy(Firewall): Add Add-FGTFirewallPolicy for add Policy Rules
    You can configure name, port source/destination, ip source/destination, status, service, schedule...

--- PowerFGT/Public/cmdb/firewall/policy.ps1
+++ PowerFGT/Public/cmdb/firewall/policy.ps1
@@ -5,6 +5,216 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function Add-FGTFirewallPolicy {
+
+    <#
+        .SYNOPSIS
+        Add a FortiGate Policy
+
+        .DESCRIPTION
+        Add a FortiGate Policy/Rules (source port/ip, destination port, ip, action, status...)
+
+        .EXAMPLE
+        Add-FGTFirewallPolicy -name test_policy -srcintf port1 -dstintf port2 -srcaddr all -dstaddr all
+
+        Add a Test_policy with source port port1 and destination port1 and source and destination all
+
+        .EXAMPLE
+        Add-FGTFirewallPolicy -name test_policy -srcintf port1 -dstintf port2 -srcaddr all -dstaddr all -nat
+
+        Add a Test_policy with NAT is enable
+
+        .EXAMPLE
+        Add-FGTFirewallPolicy -name test_policy -srcintf port1 -dstintf port2 -srcaddr all -dstaddr all -action "deny"
+
+        Add a Test_policy with action is Deny
+
+        .EXAMPLE
+        Add-FGTFirewallPolicy -name test_policy -srcintf port1 -dstintf port2 -srcaddr all -dstaddr all -status:$false
+
+        Add a Test_policy with status is disable
+
+        .EXAMPLE
+        Add-FGTFirewallPolicy -name test_policy -srcintf port1 -dstintf port2 -srcaddr all -dstaddr all -service "HTTP, HTTPS, SSH"
+
+        Add a Test_policy with multiple service port
+
+        .EXAMPLE
+        Add-FGTFirewallPolicy -name test_policy -srcintf port1 -dstintf port2 -srcaddr all -dstaddr all -schedule workhour
+
+        Add a Test_policy with schedule is workhour
+
+    #>
+
+
+    Param(
+        [Parameter (Mandatory = $true)]
+        [string]$name,
+        [Parameter (Mandatory = $true)]
+        [string]$srcintf,
+        [Parameter (Mandatory = $true)]
+        [string]$dstintf,
+        [Parameter (Mandatory = $true)]
+        [string]$srcaddr,
+        [Parameter (Mandatory = $true)]
+        [string]$dstaddr,
+        [Parameter (Mandatory = $false)]
+        [ValidateSet("accept", "deny", "learn")]
+        [string]$action = "accept",
+        [Parameter (Mandatory = $false)]
+        [switch]$status = $true,
+        [Parameter (Mandatory = $false)]
+        [string]$schedule = "always",
+        [Parameter (Mandatory = $false)]
+        [string]$service = "ALL",
+        [Parameter (Mandatory = $false)]
+        [switch]$nat = $false,
+        [Parameter (Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+
+        if ( Get-FGTFirewallPolicy -connection $connection @invokeParams -name $name ) {
+            Throw "Already a Policy using the same name"
+        }
+
+
+        $uri = "api/v2/cmdb/firewall/policy"
+
+        # SOURCE INTERFACE
+        $arr = $srcintf -split ', '
+        $countarr = $arr.count
+        $line = 0
+        while ($line -ne $countarr) {
+
+            New-Variable -Name "srcintf_array$line" -Value $arr[$line]
+            $temp = Get-Variable -Name "srcintf_array$line" -ValueOnly
+            $temp = new-Object -TypeName PSObject
+            $temp | add-member -name "name" -membertype NoteProperty -Value $arr[$line]
+            [array]$srcintf_array_array += $temp
+            $line = $line + 1
+
+        }
+
+        # DESTINATION INTERFACE
+        $arr = $dstintf -split ', '
+        $countarr = $arr.count
+        $line = 0
+        while ($line -ne $countarr) {
+
+            New-Variable -Name "dstintf_array$line" -Value $arr[$line]
+            $temp = Get-Variable -Name "dstintf_array$line" -ValueOnly
+            $temp = new-Object -TypeName PSObject
+            $temp | add-member -name "name" -membertype NoteProperty -Value $arr[$line]
+            [array]$dstintf_array_array += $temp
+            $line = $line + 1
+
+        }
+
+        # SOURCE ADDRESSE
+        $arr = $srcaddr -split ', '
+        $countarr = $arr.count
+        $line = 0
+        while ($line -ne $countarr) {
+
+            New-Variable -Name "srcaddr_array$line" -Value $arr[$line]
+            $temp = Get-Variable -Name "srcaddr_array$line" -ValueOnly
+            $temp = new-Object -TypeName PSObject
+            $temp | add-member -name "name" -membertype NoteProperty -Value $arr[$line]
+            [array]$srcaddr_array_array += $temp
+            $line = $line + 1
+
+        }
+
+        # DESTINATION ADDRESSE
+        $arr = $dstaddr -split ', '
+        $countarr = $arr.count
+        $line = 0
+        while ($line -ne $countarr) {
+
+            New-Variable -Name "dstaddr_array$line" -Value $arr[$line]
+            $temp = Get-Variable -Name "dstaddr_array$line" -ValueOnly
+            $temp = new-Object -TypeName PSObject
+            $temp | add-member -name "name" -membertype NoteProperty -Value $arr[$line]
+            [array]$dstaddr_array_array += $temp
+            $line = $line + 1
+
+        }
+
+        # SERVICE
+        $arr = $service -split ', '
+        $countarr = $arr.count
+        $line = 0
+        while ($line -ne $countarr) {
+
+            New-Variable -Name "service_array$line" -Value $arr[$line]
+            $temp = Get-Variable -Name "service_array$line" -ValueOnly
+            $temp = new-Object -TypeName PSObject
+            $temp | add-member -name "name" -membertype NoteProperty -Value $arr[$line]
+            [array]$service_array_array += $temp
+            $line = $line + 1
+
+        }
+
+        $policy = new-Object -TypeName PSObject
+
+        $policy | add-member -name "name" -membertype NoteProperty -Value $name
+
+        $policy | add-member -name "srcintf" -membertype NoteProperty -Value $srcintf_array_array
+
+        $policy | add-member -name "dstintf" -membertype NoteProperty -Value $dstintf_array_array
+
+        $policy | add-member -name "srcaddr" -membertype NoteProperty -Value $srcaddr_array_array
+
+        $policy | add-member -name "dstaddr" -membertype NoteProperty -Value $dstaddr_array_array
+
+        $policy | add-member -name "action" -membertype NoteProperty -Value $action
+
+        if ($status) {
+            $policy | add-member -name "status" -membertype NoteProperty -Value "enable"
+        }
+        else {
+            $policy | add-member -name "status" -membertype NoteProperty -Value "disable"
+        }
+
+        $policy | add-member -name "schedule" -membertype NoteProperty -Value $schedule
+
+        $policy | add-member -name "service" -membertype NoteProperty -Value $service_array_array
+
+        if ($nat) {
+            $policy | add-member -name "nat" -membertype NoteProperty -Value "enable"
+        }
+        else {
+
+            $policy | add-member -name "nat" -membertype NoteProperty -Value "disable"
+        }
+
+        Invoke-FGTRestMethod -method "POST" -body $policy -uri $uri -connection $connection @invokeParams | out-Null
+
+        Get-FGTFirewallPolicy -name $name -connection $connection @invokeParams
+
+    }
+
+    End {
+    }
+}
 function Get-FGTFirewallPolicy {
 
     <#
