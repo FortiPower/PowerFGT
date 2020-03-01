84aa9cf0686edd0dbc217ac19c17b12ff4bacebc
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-03-01 11:44:07 +0100

    Monitor(Backup): Add Get-FGTMonitorSystemConfigBackup

--- PowerFGT/PowerFGT.psm1
+++ PowerFGT/PowerFGT.psm1
@@ -7,11 +7,12 @@ $Public_cmdb_system = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\system\*.
 $Public_cmdb_user = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\user\*.ps1 -ErrorAction SilentlyContinue )
 $Public_cmdb_router = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\router\*.ps1 -ErrorAction SilentlyContinue )
 $Public_cmdb_vpnipsec = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\vpn\ipsec\*.ps1 -ErrorAction SilentlyContinue )
-$Public_monitor = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\*.ps1 -ErrorAction SilentlyContinue )
+$Public_monitor = @( Get-ChildItem -Path $PSScriptRoot\Public\monitor\*.ps1 -ErrorAction SilentlyContinue )
+$Public_monitor_system_config = @( Get-ChildItem -Path $PSScriptRoot\Public\monitor\system\config\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 #Dot source the files
-Foreach ($import in @($Public + $Public_cmdb + $Public_cmdb_firewall + $Public_cmdb_firewall_service + $Public_cmdb_system + $Public_cmdb_user + $Public_cmdb_router + $Public_cmdb_vpnipsec + $Public_monitor + $Private)) {
+Foreach ($import in @($Public + $Public_cmdb + $Public_cmdb_firewall + $Public_cmdb_firewall_service + $Public_cmdb_system + $Public_cmdb_user + $Public_cmdb_router + $Public_cmdb_vpnipsec + $Public_monitor + $Public_monitor_system_config + $Private)) {
     Try {
         . $import.fullname
     }
---
+++ PowerFGT/Public/monitor/system/config/backup.ps1
@@ -0,0 +1,60 @@
+﻿#
+# Copyright 2020, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Get-FGTMonitorSystemConfigBackup {
+
+    <#
+        .SYNOPSIS
+        Get System Config Backup
+
+        .DESCRIPTION
+        Get System Config Backup
+
+        .EXAMPLE
+        Get-FGTMonitorSystemConfigBackup
+
+        Get System Config Backup
+
+        .EXAMPLE
+        Get-FGTSystemInterface -skip
+
+        Get list of all interface (but only relevant attributes)
+
+        .EXAMPLE
+        Get-FGTSystemInterface -vdom vdomX
+
+        Get list of all interface on vdomX
+    #>
+
+    Param(
+        [Parameter(Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        $uri = 'api/v2/monitor/system/config/backup?scope=global'
+        $response = Invoke-FGTRestMethod -uri $uri -method 'GET' -connection $connection @invokeParams
+        $response.results
+    }
+
+    End {
+    }
+}
