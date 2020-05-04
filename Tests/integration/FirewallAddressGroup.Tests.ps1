4eacdf15b1a9160c759381e6013ac39fee04b0c8
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-05-04 11:42:13 +0200

    FirewallAdressGroup(Tests): Add Test for Add Firewall Address Group

--- Tests/integration/FirewallAddressGroup.Tests.ps1
+++ Tests/integration/FirewallAddressGroup.Tests.ps1
@@ -71,4 +71,71 @@ Describe "Get Firewall Address Group" {
 
 }
 
+Describe "Add Firewall Address Group" {
+
+    BeforeAll {
+        #Create some Address object
+        Add-FGTFirewallAddress -type ipmask -Name $pester_address1 -ip 192.0.2.1 -mask 255.255.255.255
+        Add-FGTFirewallAddress -type ipmask -Name $pester_address2 -ip 192.0.2.2 -mask 255.255.255.255
+    }
+
+    AfterEach {
+        Get-FGTFirewallAddressGroup -name $pester_addressgroup | Remove-FGTFirewallAddressGroup -noconfirm
+    }
+
+    It "Add Address Group $pester_addressgroup (with 1 member)" {
+        Add-FGTFirewallAddressGroup -Name $pester_addressgroup -member $pester_address1
+        $addressgroup = Get-FGTFirewallAddressGroup -name $pester_addressgroup
+        $addressgroup.name | Should -Be $pester_addressgroup
+        $addressgroup.uuid | Should -Not -BeNullOrEmpty
+        ($addressgroup.member).count | Should -Be "1"
+        $addressgroup.comment | Should -BeNullOrEmpty
+        $addressgroup.visibility | Should -Be $true
+    }
+
+    It "Add Address Group $pester_addressgroup (with 1 member and a comment)" {
+        Add-FGTFirewallAddressGroup -Name $pester_addressgroup -member $pester_address1 -comment "Add via PowerFGT"
+        $addressgroup = Get-FGTFirewallAddressGroup -name $pester_addressgroup
+        $addressgroup.name | Should -Be $pester_addressgroup
+        $addressgroup.uuid | Should -Not -BeNullOrEmpty
+        ($addressgroup.member).count | Should -Be "1"
+        $addressgroup.comment | Should -Be "Add via PowerFGT"
+        $addressgroup.visibility | Should -Be $true
+    }
+
+    It "Add Address Group $pester_addressgroup (with 1 member and visibility disable)" {
+        Add-FGTFirewallAddressGroup -Name $pester_addressgroup -member $pester_address1 -visibility:$false
+        $addressgroup = Get-FGTFirewallAddressGroup -name $pester_addressgroup
+        $addressgroup.name | Should -Be $pester_addressgroup
+        $addressgroup.uuid | Should -Not -BeNullOrEmpty
+        ($addressgroup.member).count | Should -Be "1"
+        $addressgroup.comment | Should -BeNullOrEmpty
+        $addressgroup.visibility | Should -Be "disable"
+    }
+
+    It "Add Address Group $pester_addressgroup (with 2 members)" {
+        Add-FGTFirewallAddressGroup -Name $pester_addressgroup -member $pester_address1, $pester_address2
+        $addressgroup = Get-FGTFirewallAddressGroup -name $pester_addressgroup
+        $addressgroup.name | Should -Be $pester_addressgroup
+        $addressgroup.uuid | Should -Not -BeNullOrEmpty
+        ($addressgroup.member).count | Should -Be "2"
+        $addressgroup.comment | Should -BeNullOrEmpty
+        $addressgroup.visibility | Should -Be $true
+    }
+
+    It "Try to Add Address Group $pester_addressgroup (but there is already a object with same name)" {
+        #Add first Address Group
+        Add-FGTFirewallAddressGroup -Name $pester_addressgroup -member $pester_address1
+        #Add Second Address Group with same name
+        { Add-FGTFirewallAddressGroup -Name $pester_addressgroup -member $pester_address1 } | Should -Throw "Already an addressgroup object using the same name"
+
+    }
+
+    AfterAll {
+        Get-FGTFirewallAddress -name $pester_address1 | Remove-FGTFirewallAddress -noconfirm
+        Get-FGTFirewallAddress -name $pester_address2 | Remove-FGTFirewallAddress -noconfirm
+    }
+
+}
+
 Disconnect-FGT -noconfirm
