d849862f70d4e71c755f843300130af64a0a3203
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-05-07 12:44:48 +0200

    Connection(Tests): use Pester v4 Synthax

--- Tests/integration/Connection.Tests.ps1
+++ Tests/integration/Connection.Tests.ps1
@@ -67,66 +67,66 @@ Describe "Connect to a FortiGate (using multi connection)" {
     }
 
     It "Throw when try to use Invoke-FGTRestMethod and not connected" {
-        { Invoke-FGTRestMethod -uri "api/v2/cmdb/firewall/address" } | Should throw "Not Connected. Connect to the Fortigate with Connect-FGT"
+        { Invoke-FGTRestMethod -uri "api/v2/cmdb/firewall/address" } | Should -Throw "Not Connected. Connect to the Fortigate with Connect-FGT"
     }
 
     Context "Use Multi connection for call some (Get) cmdlet (Vlan, System...)" {
         It "Use Multi connection for call Get Firewall Address" {
-            { Get-FGTFirewallAddress -connection $fgt } | Should Not throw
+            { Get-FGTFirewallAddress -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get Firewall Address Group" {
-            { Get-FGTFirewallAddressGroup  -connection $fgt } | Should Not throw
+            { Get-FGTFirewallAddressGroup  -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get Firewall IP Pool" {
-            { Get-FGTFirewallIPPool -connection $fgt } | Should Not throw
+            { Get-FGTFirewallIPPool -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get Firewall Policy" {
-            { Get-FGTFirewallPolicy -connection $fgt } | Should Not throw
+            { Get-FGTFirewallPolicy -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get Firewall Service Custom" {
-            { Get-FGTFirewallServiceCustom -connection $fgt } | Should Not throw
+            { Get-FGTFirewallServiceCustom -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get Firewall Service Group" {
-            { Get-FGTFirewallServiceGroup -connection $fgt } | Should Not throw
+            { Get-FGTFirewallServiceGroup -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get Firewall Virtual IP (VIP)" {
-            { Get-FGTFirewallVip -connection $fgt } | Should Not throw
+            { Get-FGTFirewallVip -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get Router Policy" {
-            { Get-FGTRouterPolicy -connection $fgt } | Should Not throw
+            { Get-FGTRouterPolicy -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get Router Static" {
-            { Get-FGTRouterStatic -connection $fgt } | Should Not throw
+            { Get-FGTRouterStatic -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get System DNS" {
-            { Get-FGTSystemDns -connection $fgt } | Should Not throw
+            { Get-FGTSystemDns -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get System Global" {
-            { Get-FGTSystemGlobal -connection $fgt } | Should Not throw
+            { Get-FGTSystemGlobal -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get System HA" {
-            { Get-FGTSystemHA -connection $fgt } | Should Not throw
+            { Get-FGTSystemHA -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get System Interface" {
-            { Get-FGTSystemInterface -connection $fgt } | Should Not throw
+            { Get-FGTSystemInterface -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get System Vdom" {
-            { Get-FGTSystemVdom -connection $fgt } | Should Not throw
+            { Get-FGTSystemVdom -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get System Virtual WAN Link (SD-WAN)" {
-            { Get-FGTSystemVirtualWANLink -connection $fgt } | Should Not throw
+            { Get-FGTSystemVirtualWANLink -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get System Zone " {
-            { Get-FGTSystemZone -connection $fgt } | Should Not throw
+            { Get-FGTSystemZone -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get User Local" {
-            { Get-FGTUserLocal -connection $fgt } | Should Not throw
+            { Get-FGTUserLocal -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get VPN IPsec Phase 1 Interface" {
-            { Get-FGTVpnIpsecPhase1Interface -connection $fgt } | Should Not throw
+            { Get-FGTVpnIpsecPhase1Interface -connection $fgt } | Should -Not -Throw
         }
         It "Use Multi connection for call Get VPN IPsec Phase 2 Interface" {
-            { Get-FGTVpnIpsecPhase2Interface -connection $fgt } | Should Not throw
+            { Get-FGTVpnIpsecPhase2Interface -connection $fgt } | Should -Not -Throw
         }
     }
 
