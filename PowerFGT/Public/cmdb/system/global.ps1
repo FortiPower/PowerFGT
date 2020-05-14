777e6669eee629b782ac413fdc7bfa2b7aaf96c2
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-05-14 14:27:54 +0200

    Global(System): Add Set-FGTSystemGlobal for modified Global Settings
    hostname, alias, timezone, dst...

--- PowerFGT/Public/cmdb/system/global.ps1
+++ PowerFGT/Public/cmdb/system/global.ps1
@@ -87,3 +87,98 @@ function Get-FGTSystemGlobal {
     End {
     }
 }
+
+function Set-FGTSystemGlobal {
+
+    <#
+        .SYNOPSIS
+        Configure a FortiGate System Global
+
+        .DESCRIPTION
+        Change a FortiGate System Global Settings (hostname, alias....)
+
+        .EXAMPLE
+        Set-FGTSystemGlobal -hostname MyFGT
+
+        Change hostname to value MyFGT
+
+        .EXAMPLE
+        Set-FGTSystemGlobal -alias MyFGT
+
+        Change alias to value MyFGT
+
+        .EXAMPLE
+        Set-FGTSystemGlobal -timezone 28
+
+        Change timezone to 28 (GMT+1:00) Brussels, Copenhagen, Madrid, Paris
+    #>
+
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'medium')]
+    Param(
+        [Parameter (Mandatory = $false)]
+        [string]$hostname,
+        [Parameter (Mandatory = $false)]
+        [string]$alias,
+        [Parameter (Mandatory = $false)]
+        [ValidateRange("00", "86")]
+        [int]$timezone,
+        [Parameter (Mandatory = $false)]
+        [switch]$dst,
+        [Parameter (Mandatory = $false)]
+        [ValidateRange("1", "480")]
+        [int]$admintimeout,
+        [Parameter (Mandatory = $false)]
+        [switch]$lldp_transmission,
+        [Parameter (Mandatory = $false)]
+        [switch]$lldp_reception,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        $uri = "api/v2/cmdb/system/global"
+
+        $_sg = new-Object -TypeName PSObject
+
+        if ( $PsBoundParameters.ContainsKey('hostname') ) {
+            $_sg | add-member -name "hostname" -membertype NoteProperty -Value $hostname
+        }
+
+        if ( $PsBoundParameters.ContainsKey('alias') ) {
+            $_sg | add-member -name "alias" -membertype NoteProperty -Value $alias
+        }
+
+        if ( $PsBoundParameters.ContainsKey('timezone') ) {
+            $_sg | add-member -name "timezone" -membertype NoteProperty -Value $timezone
+        }
+
+        if ( $PsBoundParameters.ContainsKey('dst') ) {
+            if ($dst) {
+                $_sg | Add-member -name "dst" -membertype NoteProperty -Value $true
+            }
+            else {
+                $_sg | Add-member -name "dst" -membertype NoteProperty -Value $false
+            }
+        }
+
+        if ($PSCmdlet.ShouldProcess("Global", 'Configure Settings')) {
+            Invoke-FGTRestMethod -method "PUT" -body $_sg -uri $uri -connection $connection @invokeParams
+        }
+
+        Get-FGTSystemGlobal
+    }
+
+    End {
+    }
+}
