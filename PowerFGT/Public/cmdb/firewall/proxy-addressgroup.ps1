067ddf5e3b63ae1aabcc0f9367705dda97da0dec
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-06-11 08:36:54 +0200

    FirewallProxyAddressGroup: Fix when there is only one member need to be force to be an array

--- PowerFGT/Public/cmdb/firewall/proxy-addressgroup.ps1
+++ PowerFGT/Public/cmdb/firewall/proxy-addressgroup.ps1
@@ -560,6 +560,11 @@ function Remove-FGTFirewallProxyAddressGroupMember {
                 Throw "You can't remove all members. Use Remove-FGTFirewallAddressGroup to remove Address Group"
             }
 
+            #if there is only One member force to be an array
+            if ( $members.count -eq 1 ) {
+                $members = @($members)
+            }
+
             $_addrgrp | add-member -name "member" -membertype NoteProperty -Value $members
         }
 
