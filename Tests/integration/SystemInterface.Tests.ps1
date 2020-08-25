83c4428edbeb7ccdd3d6fac3779fbe15d8e42e15
Cédric Moreau <moreaucedric0@gmail.com>
2020-08-25 12:07:28 +0200

    Fix trailing whitespace, add variables in tests

--- Tests/integration/SystemInterface.Tests.ps1
+++ Tests/integration/SystemInterface.Tests.ps1
@@ -10,10 +10,10 @@
 Describe "Get System Interface" {
 
     BeforeAll {
-        Add-FGTSystemInterface -name PowerFGT -type vlan -role lan -mode static -vdom_interface root -interface port10 -vlan_id 10
+        Add-FGTSystemInterface -name $pester_zone1 -type vlan -role lan -mode static -vdom_interface root -interface $pester_port1 -vlan_id 10
     }
     AfterAll {
-        $remove = Get-FGTSystemInterface -name PowerFGT 
+        $remove = Get-FGTSystemInterface -name $pester_zone1
         $remove.name | Remove-FGTSystemInterface -confirm:$false
     }
 
@@ -43,19 +43,19 @@ Describe "Get System Interface" {
 Describe "Add System Interface" {
 
     AfterEach {
-        $remove = Get-FGTSystemInterface -name PowerFGT 
+        $remove = Get-FGTSystemInterface -name $pester_zone1
         $remove.name | Remove-FGTSystemInterface -confirm:$false
     }
 
     It "Add System Interface" {
-        Add-FGTSystemInterface -name PowerFGT -type vlan -alias Alias_PowerFGT -role lan -vlan_id 10 -interface port10 -admin_access https,ping,ssh -connected $true -device_identification $true -mode static -address_mask 192.0.2.1/255.255.255.0 -vdom_interface root
-        $interface = Get-FGTSystemInterface -name PowerFGT
-        $interface.name | Should -Be "PowerFGT"
+        Add-FGTSystemInterface -name $pester_zone1 -type vlan -alias Alias_$pester_zone1 -role lan -vlan_id 10 -interface $pester_port1 -admin_access https,ping,ssh -connected $true -device_identification $true -mode static -address_mask 192.0.2.1/255.255.255.0 -vdom_interface root
+        $interface = Get-FGTSystemInterface -name $pester_zone1
+        $interface.name | Should -Be $pester_zone1
         $interface.type | Should -Be "vlan"
-        $interface.alias | Should -Be "Alias_PowerFGT"
+        $interface.alias | Should -Be "Alias_$pester_zone1"
         $interface.role | Should -Be "lan"
         $interface.vlanid | Should -Be 10
-        $interface.interface | Should -Be "port10"
+        $interface.interface | Should -Be $pester_port1
         $interface.allowaccess | Should -Be "ping https ssh"
         $interface.status | Should -Be "up"
         $interface."device-identification" | Should -Be "enable"
@@ -64,13 +64,13 @@ Describe "Add System Interface" {
     }
 
     It "Add System Interface with only mandatory parameters" {
-        Add-FGTSystemInterface -name PowerFGT -type vlan -role lan -mode static -vdom_interface root -interface port10 -vlan_id 10
-        $interface = Get-FGTSystemInterface -name PowerFGT
-        $interface.name | Should -Be "PowerFGT"
+        Add-FGTSystemInterface -name $pester_zone1 -type vlan -role lan -mode static -vdom_interface root -interface $pester_port1 -vlan_id 10
+        $interface = Get-FGTSystemInterface -name $pester_zone1
+        $interface.name | Should -Be $pester_zone1
         $interface.type | Should -Be "vlan"
         $interface.role | Should -Be "lan"
         $interface.vlanid | Should -Be 10
-        $interface.interface | Should -Be "port10"
+        $interface.interface | Should -Be $pester_port1
         $interface.mode | Should -Be "static"
     }
 
@@ -79,52 +79,52 @@ Describe "Add System Interface" {
 Describe "Set System Interface" {
 
     BeforeAll {
-        Add-FGTSystemInterface -name PowerFGT -type vlan -role lan -mode static -vdom_interface root -interface port10 -vlan_id 10
+        Add-FGTSystemInterface -name $pester_zone1 -type vlan -role lan -mode static -vdom_interface root -interface $pester_port1 -vlan_id 10
     }
     AfterAll {
-        $remove = Get-FGTSystemInterface -name PowerFGT 
+        $remove = Get-FGTSystemInterface -name $pester_zone1
         $remove.name | Remove-FGTSystemInterface -confirm:$false
     }
 
     It "Set System Interface alias" {
-        Set-FGTSystemInterface -name PowerFGT -alias Set_PowerFGT
-        $interface = Get-FGTSystemInterface -name PowerFGT
-        $interface.alias | Should -Be "Set_PowerFGT"
+        Set-FGTSystemInterface -name $pester_zone1 -alias Set_$pester_zone1
+        $interface = Get-FGTSystemInterface -name $pester_zone1
+        $interface.alias | Should -Be "Set_$pester_zone1"
     }
 
     It "Set System Interface role" {
-        Set-FGTSystemInterface -name PowerFGT -role dmz
-        $interface = Get-FGTSystemInterface -name PowerFGT
+        Set-FGTSystemInterface -name $pester_zone1 -role dmz
+        $interface = Get-FGTSystemInterface -name $pester_zone1
         $interface.role | Should -Be "dmz"
     }
 
     It "Set System Interface ip" {
-        Set-FGTSystemInterface -name PowerFGT -address_mask 192.0.2.1/255.255.255.0
-        $interface = Get-FGTSystemInterface -name PowerFGT
+        Set-FGTSystemInterface -name $pester_zone1 -address_mask 192.0.2.1/255.255.255.0
+        $interface = Get-FGTSystemInterface -name $pester_zone1
         $interface.ip | Should -Be "192.0.2.1 255.255.255.0"
     }
 
     It "Set System Interface connected" {
-        Set-FGTSystemInterface -name PowerFGT -connected $true
-        $interface = Get-FGTSystemInterface -name PowerFGT
+        Set-FGTSystemInterface -name $pester_zone1 -connected $true
+        $interface = Get-FGTSystemInterface -name $pester_zone1
         $interface.status | Should -Be "up"
     }
 
     It "Set System Interface device-identification" {
-        Set-FGTSystemInterface -name PowerFGT -device_identification $true
-        $interface = Get-FGTSystemInterface -name PowerFGT
+        Set-FGTSystemInterface -name $pester_zone1 -device_identification $true
+        $interface = Get-FGTSystemInterface -name $pester_zone1
         $interface."device-identification" | Should -Be "enable"
     }
 
     It "Set System Interface mode" {
-        Set-FGTSystemInterface -name PowerFGT -mode dhcp
-        $interface = Get-FGTSystemInterface -name PowerFGT
+        Set-FGTSystemInterface -name $pester_zone1 -mode dhcp
+        $interface = Get-FGTSystemInterface -name $pester_zone1
         $interface.mode | Should -Be "dhcp"
     }
 
     It "Set System Interface administrative access" {
-        Set-FGTSystemInterface -name PowerFGT -admin_access https,ssh
-        $interface = Get-FGTSystemInterface -name PowerFGT
+        Set-FGTSystemInterface -name $pester_zone1 -admin_access https,ssh
+        $interface = Get-FGTSystemInterface -name $pester_zone1
         $interface.allowaccess | Should -Be "https ssh"
     }
 
@@ -133,19 +133,19 @@ Describe "Set System Interface" {
 Describe "Remove System Interface" {
 
     BeforeEach {
-        Add-FGTSystemInterface -name PowerFGT -type vlan -role lan -mode static -vdom_interface root -interface port10 -vlan_id 10
+        Add-FGTSystemInterface -name $pester_zone1 -type vlan -role lan -mode static -vdom_interface root -interface $pester_port1 -vlan_id 10
     }
 
     It "Remove System Interface by name" {
-        Remove-FGTSystemInterface -name PowerFGT -confirm:$false
-        $interface = Get-FGTSystemInterface -name PowerFGT
+        Remove-FGTSystemInterface -name $pester_zone1 -confirm:$false
+        $interface = Get-FGTSystemInterface -name $pester_zone1
         $interface | Should -Be $NULL
     }
 
     It "Remove System Interface by pipeline" {
-        $interface = Get-FGTSystemInterface -name PowerFGT
+        $interface = Get-FGTSystemInterface -name $pester_zone1
         $interface.name | Remove-FGTSystemInterface -confirm:$false
-        $interface = Get-FGTSystemInterface -name PowerFGT
+        $interface = Get-FGTSystemInterface -name $pester_zone1
         $interface | Should -Be $NULL
     }
 
