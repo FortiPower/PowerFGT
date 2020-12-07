0ff51b2e9582a27b916a61b078028e856cad8a59
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-12-07 07:55:32 +0100

    Virtual WAN Link: use sdwan uri when use FortiOS 6.4.x
    add Warning about use Get-FGTSystemSDWAN for FortiOS 6.4.x

--- PowerFGT/Public/cmdb/system/virtual-wan-link.ps1
+++ PowerFGT/Public/cmdb/system/virtual-wan-link.ps1
@@ -64,6 +64,8 @@ function Get-FGTSystemVirtualWANLink {
 
     Process {
 
+        $uri = 'api/v2/cmdb/system/virtual-wan-link'
+
         $invokeParams = @{ }
         if ( $PsBoundParameters.ContainsKey('skip') ) {
             $invokeParams.add( 'skip', $skip )
@@ -80,7 +82,13 @@ function Get-FGTSystemVirtualWANLink {
             $invokeParams.add( 'filter_type', $filter_type )
         }
 
-        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/system/virtual-wan-link' -method 'GET' -connection $connection @invokeParams
+        #with 6.4.x, it is a new uri -> SD-WAN
+        if ($connection.version -ge "6.4.0") {
+            $uri = "api/v2/cmdb/system/sdwan"
+            Write-Warning "Please use Get-FGTSystemSDWAN, Virtual WAN Link is not longer available with FortiOS 6.4.x and after"
+        }
+
+        $reponse = Invoke-FGTRestMethod -uri $uri -method 'GET' -connection $connection @invokeParams
         $reponse.results
     }
 
