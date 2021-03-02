3859df353d8f36bb247f16162a94056a40a32bb5
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-03-03 00:33:41 +0100

    Traffic(Log): Add Get-FGTLogTraffic for get Log Traffic
    you can get log from memory, disk, forticloud and fortimanager
    and get subtype forward, local, multicast, sniffer, fortiview, threat
    
    can you also filter by srcip, srcintf, dstip, dstinf, dstport, action and policyid

--- PowerFGT/PowerFGT.psm1
+++ PowerFGT/PowerFGT.psm1
@@ -9,10 +9,11 @@ $Public_cmdb_user = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\user\*.ps1 
 $Public_cmdb_router = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\router\*.ps1 -ErrorAction SilentlyContinue )
 $Public_cmdb_vpnipsec = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\vpn\ipsec\*.ps1 -ErrorAction SilentlyContinue )
 $Public_monitor = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\*.ps1 -ErrorAction SilentlyContinue )
+$Public_log = @( Get-ChildItem -Path $PSScriptRoot\Public\log\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 #Dot source the files
-Foreach ($import in @($Public + $Public_cmdb + $Public_cmdb_firewall + $Public_cmdb_firewall_proxy + $Public_cmdb_firewall_service + $Public_cmdb_system + $Public_cmdb_user + $Public_cmdb_router + $Public_cmdb_vpnipsec + $Public_monitor + $Private)) {
+Foreach ($import in @($Public + $Public_cmdb + $Public_cmdb_firewall + $Public_cmdb_firewall_proxy + $Public_cmdb_firewall_service + $Public_cmdb_system + $Public_cmdb_user + $Public_cmdb_router + $Public_cmdb_vpnipsec + $Public_monitor + $Public_log + $Private)) {
     Try {
         . $import.fullname
     }
---
+++ PowerFGT/Public/log/traffic.ps1
@@ -0,0 +1,151 @@
+#
+# Copyright 2021, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Get-FGTLogTraffic {
+
+    <#
+        .SYNOPSIS
+        Get System Config Backup
+
+        .DESCRIPTION
+        Get System Config Backup
+
+        .EXAMPLE
+        Get-FGTMonitorSystemConfigBackup
+
+        Get System Config Backup
+
+        .EXAMPLE
+        Get-FGTMonitorSystemConfigBackup -skip
+
+        Get System Config Backup (but only relevant attributes)
+
+        .EXAMPLE
+        Get-FGTMonitorSystemConfigBackup -vdom vdomX
+
+        Get System Config Backup on vdomX
+    #>
+
+    [CmdletBinding(DefaultParameterSetName = 'default')]
+    Param(
+        [Parameter (Mandatory = $true, Position = 1)]
+        [ValidateSet('disk', 'fortianalyzer', 'forticloud', 'memory', IgnoreCase = $false)]
+        [string]$type,
+        [Parameter (Mandatory = $true, Position = 2)]
+        [ValidateSet('forward', 'local', 'multicast', 'sniffer', 'fortiview', 'threat', IgnoreCase = $false)]
+        [string]$subtype,
+        [Parameter (Mandatory = $false)]
+        [int]$rows,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "srcip")]
+        [string]$srcip,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "srcintf")]
+        [string]$srcintf,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "dstip")]
+        [string]$dstip,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "dstinf")]
+        [string]$dstintf,
+        [Parameter (Mandatory = $false)]
+        [ValidateRange(0,65535)]
+        [Parameter (ParameterSetName = "dstport")]
+        [int]$dstport,
+        [Parameter (Mandatory = $false)]
+        [ValidateSet('accept', 'client-rst', 'server-rst', 'close', 'ip-conn', 'timeout', IgnoreCase = $false)]
+        [Parameter (ParameterSetName = "action")]
+        [string]$action,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "policyid")]
+        [int]$policyid,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [string]$filter_attribute,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "srcip")]
+        [Parameter (ParameterSetName = "srcintf")]
+        [Parameter (ParameterSetName = "dstip")]
+        [Parameter (ParameterSetName = "dstinf")]
+        [Parameter (ParameterSetName = "dstport")]
+        [Parameter (ParameterSetName = "action")]
+        [Parameter (ParameterSetName = "policyid")]
+        [Parameter (ParameterSetName = "filter")]
+        [ValidateSet('equal', 'contains')]
+        [string]$filter_type = "equal",
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [psobject]$filter_value,
+        [Parameter(Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+        if ( $PsBoundParameters.ContainsKey('rows') ) {
+            $invokeParams.add( 'rows', $rows )
+        }
+
+        switch ( $PSCmdlet.ParameterSetName ) {
+            "srcip" {
+                $filter_value = $srcip
+                $filter_attribute = "srcip"
+            }
+            "srcintf" {
+                $filter_value = $srcintf
+                $filter_attribute = "srcintf"
+            }
+            "dstip" {
+                $filter_value = $dstcip
+                $filter_attribute = "dstip"
+            }
+            "dstintf" {
+                $filter_value = $dstintf
+                $filter_attribute = "dstintf"
+            }
+            "dstport" {
+                $filter_value = $dstport
+                $filter_attribute = "dstport"
+            }
+            "action" {
+                $filter_value = $action
+                $filter_attribute = "action"
+            }
+            "policyid" {
+                $filter_value = $policyid
+                $filter_attribute = "policyid"
+            }
+            default { }
+        }
+
+        #if filter value and filter_attribute, add filter (by default filter_type is equal)
+        if ( $filter_value -and $filter_attribute ) {
+            $invokeParams.add( 'filter_value', $filter_value )
+            $invokeParams.add( 'filter_attribute', $filter_attribute )
+            $invokeParams.add( 'filter_type', $filter_type )
+        }
+
+        $uri = "api/v2/log/${type}/traffic/${subtype}"
+        $response = Invoke-FGTRestMethod -uri $uri -method 'GET' -connection $connection @invokeParams
+        $response.results
+    }
+
+    End {
+    }
+}
