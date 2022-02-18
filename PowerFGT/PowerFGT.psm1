fbc09358d6163d9101854624845ae4cfb25b91f2
Alexis La Goutte <alexis.lagoutte@gmail.com>
2022-02-18 17:13:04 +0100

    PowerFGT(.psm1): use -Recurse to get all file of Public folder...

--- PowerFGT/PowerFGT.psm1
+++ PowerFGT/PowerFGT.psm1
@@ -1,19 +1,9 @@
 #Get public and private function definition files.
-$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
-$Public_cmdb = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\*.ps1 -ErrorAction SilentlyContinue )
-$Public_cmdb_firewall = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\firewall\*.ps1 -ErrorAction SilentlyContinue )
-$Public_cmdb_firewall_proxy = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\firewall\proxy\*.ps1 -ErrorAction SilentlyContinue )
-$Public_cmdb_firewall_service = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\firewall\service\*.ps1 -ErrorAction SilentlyContinue )
-$Public_cmdb_system = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\system\*.ps1 -ErrorAction SilentlyContinue )
-$Public_cmdb_user = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\user\*.ps1 -ErrorAction SilentlyContinue )
-$Public_cmdb_router = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\router\*.ps1 -ErrorAction SilentlyContinue )
-$Public_cmdb_vpnipsec = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\vpn\ipsec\*.ps1 -ErrorAction SilentlyContinue )
-$Public_monitor = @( Get-ChildItem -Path $PSScriptRoot\Public\cmdb\*.ps1 -ErrorAction SilentlyContinue )
-$Public_log = @( Get-ChildItem -Path $PSScriptRoot\Public\log\*.ps1 -ErrorAction SilentlyContinue )
+$Public = @( Get-ChildItem -Recurse -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 #Dot source the files
-Foreach ($import in @($Public + $Public_cmdb + $Public_cmdb_firewall + $Public_cmdb_firewall_proxy + $Public_cmdb_firewall_service + $Public_cmdb_system + $Public_cmdb_user + $Public_cmdb_router + $Public_cmdb_vpnipsec + $Public_monitor + $Public_log + $Private)) {
+Foreach ($import in @($Public + $Private)) {
     Try {
         . $import.fullname
     }
