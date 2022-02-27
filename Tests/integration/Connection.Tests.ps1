7cf5ea0054819ef4012db4e872fe818db078e4db
Alexis La Goutte <alexis.lagoutte@gmail.com>
2022-02-27 20:47:27 +0100

    Connection(Tests): Add Multi Connection test for Get-MonitorSystemConfigBackup

--- Tests/integration/Connection.Tests.ps1
+++ Tests/integration/Connection.Tests.ps1
@@ -206,6 +206,9 @@ Describe "Connect to a FortiGate (using multi connection)" {
         It "Use Multi connection for call Get VPN IPsec Phase 2 Interface" {
             { Get-FGTVpnIpsecPhase2Interface -connection $fgt } | Should -Not -Throw
         }
+        It "Use Multi connection for call Get Monitor System Config Backup" {
+            { Get-FGTMonitorSystemConfigBackup -connection $fgt } | Should -Not -Throw
+        }
     }
 
     It "Disconnect to a FortiGate (Multi connection)" {
