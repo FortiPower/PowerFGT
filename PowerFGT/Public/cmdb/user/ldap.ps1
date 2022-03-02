446e80ffec5ddc2c668870a1e0fce3f05d16b4eb
Cool34000 <17827397+Cool34000@users.noreply.github.com>
2022-03-02 11:26:18 +0100

    New functions
    LDAP servers
    RADIUS servers
    User groups

---
+++ PowerFGT/Public/cmdb/user/ldap.ps1
@@ -0,0 +1,93 @@
+function Get-FGTUserLDAP {
+
+    <#
+        .SYNOPSIS
+        Get list of all LDAP servers
+
+        .DESCRIPTION
+        Get list of all LDAP servers
+
+        .EXAMPLE
+        Get-FGTUserLDAP
+
+        Display all LDAP servers
+
+        .EXAMPLE
+        Get-FGTUserLDAP -name FGT -filter_type contains
+
+        Get LDAP servers contains with *FGT*
+
+        .EXAMPLE
+        Get-FGTUserLDAP -skip
+
+        Display all LDAP servers (but only relevant attributes)
+
+        .EXAMPLE
+        Get-FGTUserLDAP -vdom vdomX
+
+        Display all LDAP servers on vdomX
+    #>
+
+    [CmdletBinding(DefaultParameterSetName = "default")]
+    Param(
+        [Parameter (Mandatory = $false, Position = 1, ParameterSetName = "name")]
+        [string]$name,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [string]$filter_attribute,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "name")]
+        [Parameter (ParameterSetName = "filter")]
+        [ValidateSet('equal', 'contains')]
+        [string]$filter_type = "equal",
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [psobject]$filter_value,
+        [Parameter(Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        #Filtering
+        switch ( $PSCmdlet.ParameterSetName ) {
+            "name" {
+                $filter_value = $name
+                $filter_attribute = "name"
+            }
+            "id" {
+                $filter_value = $id
+                $filter_attribute = "id"
+            }
+            default { }
+        }
+
+        #if filter value and filter_attribute, add filter (by default filter_type is equal)
+        if ( $filter_value -and $filter_attribute ) {
+            $invokeParams.add( 'filter_value', $filter_value )
+            $invokeParams.add( 'filter_attribute', $filter_attribute )
+            $invokeParams.add( 'filter_type', $filter_type )
+        }
+
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/user/ldap' -method 'GET' -connection $connection @invokeParams
+        $reponse.results
+    }
+
+    End {
+    }
+}
