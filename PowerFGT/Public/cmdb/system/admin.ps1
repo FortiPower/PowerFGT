65bd5acf9e15e7fe3b177133e86a3485a74a94a0
Cool34000 <17827397+Cool34000@users.noreply.github.com>
2022-03-02 13:57:04 +0100

    Another new function
    Get System Administrators

---
+++ PowerFGT/Public/cmdb/system/admin.ps1
@@ -0,0 +1,96 @@
+#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+# Copyright 2019, Benjamin Perrier <ben dot perrier at outlook dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Get-FGTSystemAdmin {
+
+    <#
+        .SYNOPSIS
+        Get list of all system administrators
+
+        .DESCRIPTION
+        Get list of all system administrators
+
+        .EXAMPLE
+        Get-FGTSystemAdmin
+
+        Display all system administrators
+
+        .EXAMPLE
+        Get-FGTSystemAdmin -name FGT -filter_type contains
+
+        Get system administrators contains with *FGT*
+
+        .EXAMPLE
+        Get-FGTSystemAdmin -skip
+
+        Display all system administrators (but only relevant attributes)
+
+        .EXAMPLE
+        Get-FGTSystemAdmin -vdom vdomX
+
+        Display all system administrators on vdomX
+    #>
+
+    [CmdletBinding(DefaultParameterSetName = "default")]
+    Param(
+        [Parameter (Mandatory = $false, Position = 1, ParameterSetName = "name")]
+        [string]$name,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [string]$filter_attribute,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "name")]
+        [Parameter (ParameterSetName = "filter")]
+        [ValidateSet('equal', 'contains')]
+        [string]$filter_type = "equal",
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [psobject]$filter_value,
+        [Parameter(Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        #Filtering
+        switch ( $PSCmdlet.ParameterSetName ) {
+            "name" {
+                $filter_value = $name
+                $filter_attribute = "name"
+            }
+            default { }
+        }
+
+        #if filter value and filter_attribute, add filter (by default filter_type is equal)
+        if ( $filter_value -and $filter_attribute ) {
+            $invokeParams.add( 'filter_value', $filter_value )
+            $invokeParams.add( 'filter_attribute', $filter_attribute )
+            $invokeParams.add( 'filter_type', $filter_type )
+        }
+
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/system/admin' -method 'GET' -connection $connection @invokeParams
+        $reponse.results
+    }
+
+    End {
+    }
+}
