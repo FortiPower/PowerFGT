6e777fe313baf4e81630e7c19667b230a1f337c2
Alexis La Goutte <alexis.lagoutte@gmail.com>
2022-03-24 07:14:39 +0100

    HA(Monitor): Add Get-FGTMonitorSystemHAPeer and Checksum
    With multi connection Tests

---
+++ PowerFGT/Public/monitor/system/ha.ps1
@@ -0,0 +1,90 @@
+#
+# Copyright 2022, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Get-FGTMonitorSystemHAPeer {
+
+    <#
+        .SYNOPSIS
+        Get HA Peer
+
+        .DESCRIPTION
+        Get HA Peer Information
+
+        .EXAMPLE
+        Get-FGTMonitorSystemHAPeer
+
+        Get VPN IPsec (name, incoming_bytes, outgoing_bytes, rgwy...)
+
+    #>
+
+    Param(
+        [Parameter (Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+
+        $uri = 'api/v2/monitor/system/ha-peer'
+
+        $response = Invoke-FGTRestMethod -uri $uri -method 'GET' -connection $connection @invokeParams
+        $response.results
+    }
+
+    End {
+    }
+}
+
+function Get-FGTMonitorSystemHAChecksum {
+
+    <#
+        .SYNOPSIS
+        Get HA Checksum
+
+        .DESCRIPTION
+        Get HA Checksum Information
+
+        .EXAMPLE
+        Get-FGTMonitorSystemHAChecksum
+
+        Get VPN IPsec (name, incoming_bytes, outgoing_bytes, rgwy...)
+
+    #>
+
+    Param(
+        [Parameter (Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+
+        $uri = 'api/v2/monitor/system/ha-checksums'
+
+        $response = Invoke-FGTRestMethod -uri $uri -method 'GET' -connection $connection @invokeParams
+        $response.results
+    }
+
+    End {
+    }
+}
