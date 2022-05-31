74ad428b5086bd87410dffd5cd417e5b00f5099a
Cédric Moreau <moreaucedric0@gmail.com>
2022-05-31 11:03:53 +0200

    Add pester variable

--- Tests/common.ps1
+++ Tests/common.ps1
@@ -44,6 +44,7 @@ $script:pester_vpn1_ph2 = "pester_vpn %/*?1_ph2"
 $script:pester_vpn2_ph2 = "pester_vpn2_ph2"
 $script:pester_zone1 = "pester_zone %/*?1"
 $script:pester_zone2 = "pester_zone2"
+$script:pester_userlocal = "pester_userlocal"
 
 . ../credential.ps1
 #TODO: Add check if no ipaddress/login/password info...
