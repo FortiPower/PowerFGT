7e868e04798b7f23bf836b92f8bdcfa37643e91a
Cédric Moreau <moreaucedric0@gmail.com>
2022-11-15 15:21:16 +0100

    Complete add function

--- PowerFGT/Public/cmdb/webfilter/urlfilter.ps1
+++ PowerFGT/Public/cmdb/webfilter/urlfilter.ps1
@@ -15,31 +15,26 @@ function Add-FGTWebfilterUrlfilter {
         Add a FortiGate URL Filter
 
         .EXAMPLE
-        Add-FGTFirewallVip -name myVIP1 -type static-nat -extip 192.0.2.1 -mappedip 198.51.100.1
+        Add-FGTWebfilterUrlfilter -name myURL1 -id 1 -name MyURL -comment "Added by PowerFGT"
 
-        Add VIP objet type static-nat (One to One) with name myVIP1 with external IP 192.0.2.1 and mapped IP 198.51.100.1
-
-        .EXAMPLE
-        Add-FGTFirewallVip -name myVIP2 -type static-nat -extip 192.0.2.2 -mappedip 198.51.100.2 -interface port1 -comment "My FGT VIP"
-
-        Add VIP objet type static-nat (One to One) with name myVIP1 with external IP 192.0.2.1, mapped IP 198.51.100.1, associated to interface port2 and a comment
+        Add URL Filter object named MyURL with comment
 
         .EXAMPLE
-        Add-FGTFirewallVip -name myVIP3-8080 -type static-nat -extip 192.0.2.1 -mappedip 198.51.100.1 -portforward -extport 8080
+        Add-FGTWebfilterUrlfilter -name myURL1 -id 1 -name MyURL -comment "Added by PowerFGT" -url_id 1 -url_type simple -url powerfgt.com -action allow -status enable
 
-        Add VIP objet type static-nat (One to One) with name myVIP3 with external IP 192.0.2.1 and mapped IP 198.51.100.1 with Port Forward and TCP Port 8080
+        Add URL Filter object named MyURL with an url (URL : powerfgt.com, type : simple, action : allow, status : enable)
 
         .EXAMPLE
-        Add-FGTFirewallVip -name myVIP4-5000-6000 -type static-nat -extip 192.0.2.1 -mappedip 198.51.100.1 -portforward -extport 5000 -mappedport 6000 -protocol udp
+        Add-FGTWebfilterUrlfilter -name myURL1 -id 1 -name MyURL -comment "Added by PowerFGT" -url_id 1 -url_type wildcard -url *powerfgt.com -action block -status enable
 
-        Add VIP objet type static-nat (One to One) with name myVIP3 with external IP 192.0.2.1 and mapped IP 198.51.100.1 with Port Forward and UDP Port 5000 mapped to port 6000
+        Add URL Filter object named MyURL with an url (URL : *powerfgt.com, type : wildcard, action : block, status : enable)
 
     #>
 
     Param(
         [Parameter (Mandatory = $false)]
         [string]$id,
-        [Parameter (Mandatory = $false)]
+        [Parameter (Mandatory = $true)]
         [string]$name,
         [Parameter (Mandatory = $false)]
         [string]$url_id,
@@ -75,51 +70,52 @@ function Add-FGTWebfilterUrlfilter {
         }
 
         if ( Get-FGTWebfilterUrlfilter -connection $connection @invokeParams -name $name ) {
-            Throw "Already a VIP object using the same name"
+            Throw "Already a URL profile object using the same name"
         }
 
         $uri = "api/v2/cmdb/webfilter/urlfilter"
 
-        $vip = new-Object -TypeName PSObject
+        $urlfilter = new-Object -TypeName PSObject
 
-        $vip | add-member -name "name" -membertype NoteProperty -Value $name
+        $urlfilter | add-member -name "name" -membertype NoteProperty -Value $name
 
-        $vip | add-member -name "type" -membertype NoteProperty -Value $type
-
-        $vip | add-member -name "extip" -membertype NoteProperty -Value $extip.ToString()
-
-        $range = New-Object -TypeName PSObject
-
-        $range | Add-member -name "range" -membertype NoteProperty -value $mappedip.ToString()
-        $vip | add-member -name "mappedip" -membertype NoteProperty -Value @($range)
-
-        #TODO check if the interface (zone ?) is valid
-        $vip | add-member -name "extintf" -membertype NoteProperty -Value $interface
+        $urlfilter | add-member -name "id" -membertype NoteProperty -Value $id
 
         if ( $PsBoundParameters.ContainsKey('comment') ) {
-            $vip | add-member -name "comment" -membertype NoteProperty -Value $comment
+            $_urlfilter | add-member -name "comment" -membertype NoteProperty -Value $comment
         }
 
-        if ( $PsBoundParameters.ContainsKey('portforward') -and $portforward -eq $true) {
-            #check if export is set before...
-            if ( $extport -eq "") {
-                throw "you need to set -extport when enable portforward parameter"
-            }
-            $vip | add-member -name "portforward" -membertype NoteProperty -Value "enable"
-            $vip | add-member -name "protocol" -membertype NoteProperty -Value $protocol
-            $vip | add-member -name "extport" -membertype NoteProperty -Value $extport
-            #if no mappedport use the extport
-            if ( $PsBoundParameters.ContainsKey('mappedport') ) {
-                $vip | add-member -name "mappedport" -membertype NoteProperty -Value $mappedport
-            }
-            else {
-                $vip | add-member -name "mappedport" -membertype NoteProperty -Value $extport
-            }
+        $_entry = new-Object -TypeName PSObject
+
+        if ( $PsBoundParameters.ContainsKey('url_id') ) {
+            $_entry | add-member -name "id" -membertype NoteProperty -Value $url_id
         }
 
-        Invoke-FGTRestMethod -method "POST" -body $vip -uri $uri -connection $connection @invokeParams | Out-Null
+        if ( $PsBoundParameters.ContainsKey('url_type') ) {
+            $_entry | add-member -name "type" -membertype NoteProperty -Value $url_type
+        }
 
-        Get-FGTFirewallVip -connection $connection @invokeParams -name $name
+        if ( $PsBoundParameters.ContainsKey('url') ) {
+            $_entry | add-member -name "url" -membertype NoteProperty -Value $url
+        }
+
+        if ( $PsBoundParameters.ContainsKey('action') ) {
+            $_entry | add-member -name "action" -membertype NoteProperty -Value $action
+        }
+
+        if ( $PsBoundParameters.ContainsKey('status') ) {
+            $_entry | add-member -name "status" -membertype NoteProperty -Value $status
+        }
+
+        if ( $PsBoundParameters.ContainsKey('exempt') ) {
+            $_entry | add-member -name "exempt" -membertype NoteProperty -Value $exempt
+        }
+
+        $urlfilter | add-member -name "entries" -membertype NoteProperty -Value $_entry
+
+        Invoke-FGTRestMethod -method "POST" -body $urlfilter -uri $uri -connection $connection @invokeParams | Out-Null
+
+        Get-FGTWebfilterUrlfilter -connection $connection @invokeParams -name $name
     }
 
     End {
