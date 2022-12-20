1c2e7958bb0e0158d7564bb83b5689f3aa923872
Alexis La Goutte <alexis.lagoutte@gmail.com>
2022-12-20 15:00:21 +0100

    SAML(User): Add Get User SAML

---
+++ PowerFGT/Public/cmdb/user/saml.ps1
@@ -0,0 +1,107 @@
+﻿#
+# Copyright 2022, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Get-FGTUserSaml {
+
+    <#
+        .SYNOPSIS
+        Get list of all "SAML users"
+
+        .DESCRIPTION
+        Get list of all "SAML users" (name, type, status... )
+
+        .EXAMPLE
+        Get-FGTUserSaml
+
+        Display all SAML users
+
+        .EXAMPLE
+        Get-FGTUserSaml -id 23
+
+        Get SAML user with id 23
+
+        .EXAMPLE
+        Get-FGTUserSaml -name FGT -filter_type contains
+
+        Get SAML user contains with *FGT*
+
+        .EXAMPLE
+        Get-FGTUserSaml -skip
+
+        Display all SAML users (but only relevant attributes)
+
+        .EXAMPLE
+        Get-FGTUserSaml -vdom vdomX
+
+        Display all SAML users on vdomX
+    #>
+
+    [CmdletBinding(DefaultParameterSetName = "default")]
+    Param(
+        [Parameter (Mandatory = $false, Position = 1, ParameterSetName = "name")]
+        [string]$name,
+        [Parameter (Mandatory = $false, ParameterSetName = "id")]
+        [string]$id,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [string]$filter_attribute,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "name")]
+        [Parameter (ParameterSetName = "id")]
+        [Parameter (ParameterSetName = "filter")]
+        [ValidateSet('equal', 'contains')]
+        [string]$filter_type = "equal",
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [psobject]$filter_value,
+        [Parameter(Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        #Filtering
+        switch ( $PSCmdlet.ParameterSetName ) {
+            "name" {
+                $filter_value = $name
+                $filter_attribute = "name"
+            }
+            "id" {
+                $filter_value = $id
+                $filter_attribute = "id"
+            }
+            default { }
+        }
+
+        #if filter value and filter_attribute, add filter (by default filter_type is equal)
+        if ( $filter_value -and $filter_attribute ) {
+            $invokeParams.add( 'filter_value', $filter_value )
+            $invokeParams.add( 'filter_attribute', $filter_attribute )
+            $invokeParams.add( 'filter_type', $filter_type )
+        }
+
+        $reponse = Invoke-FGTRestMethod -uri 'api/v2/cmdb/user/saml' -method 'GET' -connection $connection @invokeParams
+        $reponse.results
+    }
+
+    End {
+    }
+}
