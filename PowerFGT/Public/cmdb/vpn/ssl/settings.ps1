d64c6776d878ef4c8f1d7d95436f0378e32911d0
Alexis La Goutte <alexis.lagoutte@gmail.com>
2023-02-17 21:26:09 +0100

    Settings(ssl): Add Get-FGTVPNSSLSettings

---
+++ PowerFGT/Public/cmdb/vpn/ssl/settings.ps1
@@ -0,0 +1,86 @@
+﻿#
+# Copyright, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Get-FGTVpnSSLSettings {
+
+    <#
+        .SYNOPSIS
+        Get list of all VPN SSL Settings
+
+        .DESCRIPTION
+        Get list of all VPN SSL Settings (status, dns-server, port, server-cert ...)
+
+        .EXAMPLE
+        Get-FGTVpnSSLSettings
+
+        Get list of all settings of VPN SSL
+
+        .EXAMPLE
+        Get-FGTVpnSSLSettings -name FGT -filter_type contains
+
+        Get VPN SSL Settings contains with *FGT*
+
+        .EXAMPLE
+        Get-FGTVpnSSLSettings -skip
+
+        Get list of all settings of VPN SSL (but only relevant attributes)
+
+        .EXAMPLE
+        Get-FGTVpnSSLSettings -vdom vdomX
+
+        Get list of all settings of VPN SSL on vdomX
+    #>
+
+    Param(
+        [Parameter (Mandatory = $false)]
+        [switch]$plaintext_password,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [string]$filter_attribute,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "name")]
+        [Parameter (ParameterSetName = "filter")]
+        [ValidateSet('equal', 'contains')]
+        [string]$filter_type = "equal",
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [psobject]$filter_value,
+        [Parameter(Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $uri = 'api/v2/cmdb/vpn.ssl/settings'
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        #if filter value and filter_attribute, add filter (by default filter_type is equal)
+        if ( $filter_value -and $filter_attribute ) {
+            $invokeParams.add( 'filter_value', $filter_value )
+            $invokeParams.add( 'filter_attribute', $filter_attribute )
+            $invokeParams.add( 'filter_type', $filter_type )
+        }
+
+        $response = Invoke-FGTRestMethod -uri $uri -method 'GET' -connection $connection @invokeParams
+        $response.results
+    }
+
+    End {
+    }
+}
