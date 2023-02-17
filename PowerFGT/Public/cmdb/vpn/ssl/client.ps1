5f3ef588d276a3a2f3a512338497b7122c407726
Alexis La Goutte <alexis.lagoutte@gmail.com>
2023-02-17 21:26:34 +0100

    Client(ssl): Add Get-FGTVPNSSLClient

---
+++ PowerFGT/Public/cmdb/vpn/ssl/client.ps1
@@ -0,0 +1,103 @@
+﻿#
+# Copyright, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Get-FGTVpnSSLClient {
+
+    <#
+        .SYNOPSIS
+        Get list of all VPN SSL Client settings
+
+        .DESCRIPTION
+        Get list of all VPN SSL Client (name, interface, server ...)
+
+        .EXAMPLE
+        Get-FGTVpnSSLClient
+
+        Get list of all settings of VPN SSL Client
+
+        .EXAMPLE
+        Get-FGTVpnSSLClient -name myVpnSSLClient
+
+        Get VPN SSL Client named myVpnSSLClient
+
+        .EXAMPLE
+        Get-FGTVpnSSLClient -name FGT -filter_type contains
+
+        Get VPN SSL Client contains with *FGT*
+
+        .EXAMPLE
+        Get-FGTVpnSSLClient -skip
+
+        Get list of all settings of VPN SSL Client (but only relevant attributes)
+
+        .EXAMPLE
+        Get-FGTVpnSSLClient -vdom vdomX
+
+        Get list of all settings of VPN SSL Client on vdomX
+    #>
+
+    [CmdletBinding(DefaultParameterSetName = "default")]
+    Param(
+        [Parameter (Mandatory = $false, Position = 1, ParameterSetName = "name")]
+        [string]$name,
+        [Parameter (Mandatory = $false)]
+        [switch]$plaintext_password,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [string]$filter_attribute,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "name")]
+        [Parameter (ParameterSetName = "filter")]
+        [ValidateSet('equal', 'contains')]
+        [string]$filter_type = "equal",
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [psobject]$filter_value,
+        [Parameter(Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $uri = 'api/v2/cmdb/vpn.ssl/client'
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        #Filtering
+        switch ( $PSCmdlet.ParameterSetName ) {
+            "name" {
+                $filter_value = $name
+                $filter_attribute = "name"
+            }
+            default { }
+        }
+
+        #if filter value and filter_attribute, add filter (by default filter_type is equal)
+        if ( $filter_value -and $filter_attribute ) {
+            $invokeParams.add( 'filter_value', $filter_value )
+            $invokeParams.add( 'filter_attribute', $filter_attribute )
+            $invokeParams.add( 'filter_type', $filter_type )
+        }
+
+        $response = Invoke-FGTRestMethod -uri $uri -method 'GET' -connection $connection @invokeParams
+        $response.results
+    }
+
+    End {
+    }
+}
