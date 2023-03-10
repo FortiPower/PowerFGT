70559a269eca7d2d1e2efda0069602fc29c4a092
Alexis La Goutte <alexis.lagoutte@gmail.com>
2023-03-10 21:44:10 +0100

    Connection: Fix .EXAMPLE for license (need credential and skipcertificatecheck...)

--- PowerFGT/Public/Connection.ps1
+++ PowerFGT/Public/Connection.ps1
@@ -85,9 +85,10 @@ function Connect-FGT {
       Connect to a FortiGate with IP 192.0.2.1 and it will ask to get (Forti)Token code when connect
 
      .EXAMPLE
-      $lic = Get-Content -Raw $license_file
+      $lic = Get-Content -Raw license.lic
+      $Credential = New-Object System.Management.Automation.PSCredential("admin", (new-object System.Security.SecureString))
       $mynewpassword = ConvertTo-SecureString mypassword -AsPlainText -Force
-      Connect-FGT -Server 192.0.2.1 -New_Password $mynewsecpassword -license $lic
+      Connect-FGT -Server 192.0.2.1 -credentials $credential -New_Password $mynewsecpassword -license $lic -SkipCertificateCheck
 
       Connect to a FortiGate with IP 192.0.2.1 and upload the new license and change the password
   #>
