0759d82c3fbdb6365061514dd58bb483bd36588a
Alexis La Goutte <alexis.lagoutte@gmail.com>
2023-10-18 21:18:17 +0200

    Webfilter(Profile): add meta parameter

--- PowerFGT/Public/cmdb/webfilter/profile.ps1
+++ PowerFGT/Public/cmdb/webfilter/profile.ps1
@@ -28,6 +28,11 @@ function Get-FGTWebfilterProfile {
         Get Webfilter Profile contains with *FGT*
 
         .EXAMPLE
+        Get-FGTWebfilterProfile -meta
+
+        Get list of all settings of Webfilter Profile with metadata (q_...) like usage (q_ref)
+
+        .EXAMPLE
         Get-FGTWebfilterProfile -skip
 
         Get list of all settings of Webfilter Profile (but only relevant attributes)
@@ -54,6 +59,8 @@ function Get-FGTWebfilterProfile {
         [Parameter (ParameterSetName = "filter")]
         [psobject]$filter_value,
         [Parameter(Mandatory = $false)]
+        [switch]$meta,
+        [Parameter(Mandatory = $false)]
         [switch]$skip,
         [Parameter(Mandatory = $false)]
         [String[]]$vdom,
@@ -69,6 +76,9 @@ function Get-FGTWebfilterProfile {
         $uri = 'api/v2/cmdb/webfilter/profile'
 
         $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('meta') ) {
+            $invokeParams.add( 'meta', $meta )
+        }
         if ( $PsBoundParameters.ContainsKey('skip') ) {
             $invokeParams.add( 'skip', $skip )
         }
