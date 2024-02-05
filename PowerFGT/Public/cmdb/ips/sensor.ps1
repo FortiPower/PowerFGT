bc01ed407d7d3437d0e53afd535fa022200ee1ee
Alexis La Goutte <alexis.lagoutte@gmail.com>
2024-02-05 20:54:09 +0100

    IPS: Add cmdlet (and tsts) for Get IPS Sensor (list and config)

---
+++ PowerFGT/Public/cmdb/ips/sensor.ps1
@@ -0,0 +1,112 @@
+#
+# Copyright, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Get-FGTIpsSensor {
+
+    <#
+        .SYNOPSIS
+        Get list of all IPS Sensor settings
+
+        .DESCRIPTION
+        Get list of all IPS Sensor (name, options, ftgd-wf ...)
+
+        .EXAMPLE
+        Get-FGTIpsSensor
+
+        Get list of all settings of IPS Sensor
+
+        .EXAMPLE
+        Get-FGTIpsSensor -name myIpsSensor
+
+        Get IPS Sensor named myIpsSensor
+
+        .EXAMPLE
+        Get-FGTIpsSensor -name FGT -filter_type contains
+
+        Get IPS Sensor contains with *FGT*
+
+        .EXAMPLE
+        Get-FGTIpsSensor -meta
+
+        Get list of all settings of IPS Sensor with metadata (q_...) like usage (q_ref)
+
+        .EXAMPLE
+        Get-FGTIpsSensor -skip
+
+        Get list of all settings of IPS Sensor (but only relevant attributes)
+
+        .EXAMPLE
+        Get-FGTIpsSensor -vdom vdomX
+
+        Get list of all settings of IPS Sensor on vdomX
+    #>
+
+    [CmdletBinding(DefaultParameterSetName = "default")]
+    Param(
+        [Parameter (Mandatory = $false, Position = 1, ParameterSetName = "name")]
+        [string]$name,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [string]$filter_attribute,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "name")]
+        [Parameter (ParameterSetName = "filter")]
+        [ValidateSet('equal', 'contains')]
+        [string]$filter_type = "equal",
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [psobject]$filter_value,
+        [Parameter(Mandatory = $false)]
+        [switch]$meta,
+        [Parameter(Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $uri = 'api/v2/cmdb/ips/sensor'
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('meta') ) {
+            $invokeParams.add( 'meta', $meta )
+        }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        #Filtering
+        switch ( $PSCmdlet.ParameterSetName ) {
+            "name" {
+                $filter_value = $name
+                $filter_attribute = "name"
+            }
+            default { }
+        }
+
+        #if filter value and filter_attribute, add filter (by default filter_type is equal)
+        if ( $filter_value -and $filter_attribute ) {
+            $invokeParams.add( 'filter_value', $filter_value )
+            $invokeParams.add( 'filter_attribute', $filter_attribute )
+            $invokeParams.add( 'filter_type', $filter_type )
+        }
+
+
+        $response = Invoke-FGTRestMethod -uri $uri -method 'GET' -connection $connection @invokeParams
+        $response.results
+    }
+
+    End {
+    }
+}
