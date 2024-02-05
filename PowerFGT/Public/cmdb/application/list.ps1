b73a4423767362813045db3e7f009962f8255b42
Alexis La Goutte <alexis.lagoutte@gmail.com>
2024-02-05 18:55:10 +0100

    Application(List): Add Get-FGTApplicationList
    For get list of all Application

---
+++ PowerFGT/Public/cmdb/application/list.ps1
@@ -0,0 +1,112 @@
+#
+# Copyright, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Get-FGTApplicationList {
+
+    <#
+        .SYNOPSIS
+        Get list of all Application List settings
+
+        .DESCRIPTION
+        Get list of all Application List (name, options, ftgd-wf ...)
+
+        .EXAMPLE
+        Get-FGTApplicationList
+
+        Get list of all settings of Application List
+
+        .EXAMPLE
+        Get-FGTApplicationList -name myApplicationList
+
+        Get Application List named myApplicationList
+
+        .EXAMPLE
+        Get-FGTApplicationList -name FGT -filter_type contains
+
+        Get Application List contains with *FGT*
+
+        .EXAMPLE
+        Get-FGTApplicationList -meta
+
+        Get list of all settings of Application List with metadata (q_...) like usage (q_ref)
+
+        .EXAMPLE
+        Get-FGTApplicationList -skip
+
+        Get list of all settings of Application List (but only relevant attributes)
+
+        .EXAMPLE
+        Get-FGTApplicationList -vdom vdomX
+
+        Get list of all settings of Application List on vdomX
+    #>
+
+    [CmdletBinding(DefaultParameterSetName = "default")]
+    Param(
+        [Parameter (Mandatory = $false, Position = 1, ParameterSetName = "name")]
+        [string]$name,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [string]$filter_attribute,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "name")]
+        [Parameter (ParameterSetName = "filter")]
+        [ValidateSet('equal', 'contains')]
+        [string]$filter_type = "equal",
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [psobject]$filter_value,
+        [Parameter(Mandatory = $false)]
+        [switch]$meta,
+        [Parameter(Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $uri = 'api/v2/cmdb/application/list'
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('meta') ) {
+            $invokeParams.add( 'meta', $meta )
+        }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        #Filtering
+        switch ( $PSCmdlet.ParameterSetName ) {
+            "name" {
+                $filter_value = $name
+                $filter_attribute = "name"
+            }
+            default { }
+        }
+
+        #if filter value and filter_attribute, add filter (by default filter_type is equal)
+        if ( $filter_value -and $filter_attribute ) {
+            $invokeParams.add( 'filter_value', $filter_value )
+            $invokeParams.add( 'filter_attribute', $filter_attribute )
+            $invokeParams.add( 'filter_type', $filter_type )
+        }
+
+
+        $response = Invoke-FGTRestMethod -uri $uri -method 'GET' -connection $connection @invokeParams
+        $response.results
+    }
+
+    End {
+    }
+}
