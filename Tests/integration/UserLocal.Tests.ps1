1e57479ec7f084bddc4f5bc8c98a6f0cb646b6f7
Cédric Moreau <moreaucedric0@gmail.com>
2024-09-04 11:24:48 +0200

    Fix meta tests

--- Tests/integration/UserLocal.Tests.ps1
+++ Tests/integration/UserLocal.Tests.ps1
@@ -35,7 +35,7 @@ Describe "Get User Local" {
     }
 
     It "Get User Local with -name $pester_userlocal -meta" {
-        $userlocal = Get-FGTUserLocal -meta
+        $userlocal = Get-FGTUserLocal -meta -name $pester_userlocal
         $userlocal.count | Should -Not -Be $NULL
         $userlocal.q_ref | Should -Not -BeNullOrEmpty
         $userlocal.q_static | Should -Not -BeNullOrEmpty
