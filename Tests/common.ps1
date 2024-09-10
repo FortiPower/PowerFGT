f85f085271c89f21558c0eb3b28acc4ad6fcd433
Cédric Moreau <moreaucedric0@gmail.com>
2024-09-10 17:15:56 +0200

    Add pester variables in confirm.ps1

--- Tests/common.ps1
+++ Tests/common.ps1
@@ -62,6 +62,11 @@ $script:pester_usertacacsserver1 = "pestertacacsserver1.powerfgt"
 $script:pester_usertacacsserver2 = "pestertacacsserver2.powerfgt"
 $script:pester_usertacacsserver3 = "pestertacacsserver3.powerfgt"
 $script:pester_usertacacs_key = ConvertTo-SecureString "pester_usertacacskey" -AsPlainText -Force
+$script:pester_userradius = "pester_userradius"
+$script:pester_userradiusserver1 = "pester_userradiusserver1"
+$script:pester_userradiusserver2 = "pester_userradiusserver2"
+$script:pester_userradiusserver3 = "pester_userradiusserver3"
+$script:pester_userradius_secret = ConvertTo-SecureString "pester_userradiussecret" -AsPlainText -Force
 
 . ../credential.ps1
 #TODO: Add check if no ipaddress/login/password info...
