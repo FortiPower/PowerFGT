bbc30cc1837f485348f3e5a494f6bd84bbb28fa5
Cédric Moreau <moreaucedric0@gmail.com>
2024-09-11 14:36:58 +0200

    Set function

--- PowerFGT/Public/cmdb/user/tacacs.ps1
+++ PowerFGT/Public/cmdb/user/tacacs.ps1
@@ -295,3 +295,183 @@ function Get-FGTUserTACACS {
     End {
     }
 }
+
+function Set-FGTUserTACACS {
+
+    <#
+        .SYNOPSIS
+        Change a FortiGate TACACS Server
+
+        .DESCRIPTION
+        Change a FortiGate TACACS Server
+
+        .EXAMPLE
+        $MyFGTUserTACACS = Get-FGTUserTACACS -name MyFGTUserTACACS
+        PS C:\>$MyFGTUserTACACS | Set-FGTUserTACACS -server mynewTACACSserver
+
+        Change server name from MyFGTUserTACACS to mynewTACACSserver
+
+        .EXAMPLE
+        $MyFGTUserTACACS = Get-FGTUserTACACS -name MyFGTUserTACACS
+        PS C:\>$mykey = ConvertTo-SecureString mykey -AsPlainText -Force
+        PS C:\>$MyFGTUserTACACS | Set-FGTUserTACACS -secondary_server tacacs2.powerfgt -secondary_key $mykey
+
+        Change secondary server and key
+
+        .EXAMPLE
+        $MyFGTUserTACACS = Get-FGTUserTACACS -name MyFGTUserTACACS
+        PS C:\>$mykey = ConvertTo-SecureString mykey -AsPlainText -Force
+        PS C:\>$MyFGTUserTACACS | Set-FGTUserTACACS -tertiary_server tacacs3.powerfgt -tertiary_key $mykey
+
+        Change tertiary server and key
+
+        .EXAMPLE
+        $MyFGTUserTACACS = Get-FGTUserTACACS -name MyFGTUserTACACS
+        PS C:\>$MyFGTUserTACACS | Set-FGTUserTACACS -authorization disable
+
+        Change authorization to disable
+
+        .EXAMPLE
+        $data = @{ "port" = "10049" }
+        PS C:\>$MyFGTUserTACACS = Get-FGTUserTACACS -name MyFGTUserTACACS
+        PS C:\>$MyFGTUserTACACS | Set-FGTUserTACACS -data $data
+
+        Change port to 10049
+    #>
+
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'medium', DefaultParameterSetName = 'default')]
+    Param(
+        [Parameter (Mandatory = $true, ValueFromPipeline = $true, Position = 1)]
+        [ValidateScript( { Confirm-FGTUserTACACS $_ })]
+        [psobject]$usertacacs,
+        [Parameter (Mandatory = $false)]
+        [ValidateLength(1, 35)]
+        [string]$name,
+        [Parameter (Mandatory = $false)]
+        [ValidateLength(1, 63)]
+        [string]$server,
+        [Parameter (Mandatory = $false)]
+        [SecureString]$key,
+        [Parameter (Mandatory = $false)]
+        [ValidateLength(1, 63)]
+        [string]$secondary_server,
+        [Parameter (Mandatory = $false)]
+        [SecureString]$secondary_key,
+        [Parameter (Mandatory = $false)]
+        [ValidateLength(1, 63)]
+        [string]$tertiary_server,
+        [Parameter (Mandatory = $false)]
+        [SecureString]$tertiary_key,
+        [Parameter (Mandatory = $false)]
+        [ValidateRange(1, 65535)]
+        [int]$port,
+        [Parameter (Mandatory = $false)]
+        [ValidateSet("mschap", "chap", "pap", "ascii", "auto")]
+        [string]$authen_type,
+        [Parameter (Mandatory = $false)]
+        [ValidateSet("enable", "disable")]
+        [string]$authorization,
+        [Parameter (Mandatory = $false)]
+        [hashtable]$data,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        $uri = "api/v2/cmdb/user/tacacs+/$($usertacacs.name)"
+
+        $_tacacs = New-Object -TypeName PSObject
+
+        if ( $PsBoundParameters.ContainsKey('name') ) {
+            #TODO check if there is no already an object with this name ?
+            $_tacacs | add-member -name "name" -membertype NoteProperty -Value $name
+            $usertacacs.name = $name
+        }
+
+        if ( $PsBoundParameters.ContainsKey('server') ) {
+            $_tacacs | add-member -name "server" -membertype NoteProperty -Value $server
+        }
+
+        if ( $PsBoundParameters.ContainsKey('key') ) {
+            if (("Desktop" -eq $PSVersionTable.PsEdition) -or ($null -eq $PSVersionTable.PsEdition)) {
+                $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($key);
+                $key_secure = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr);
+                $_tacacs | add-member -name "key" -membertype NoteProperty -Value $key_secure
+            }
+            else {
+                $key_secure = ConvertFrom-SecureString -SecureString $key -AsPlainText
+                $_tacacs | add-member -name "key" -membertype NoteProperty -Value $key_secure
+            }
+        }
+
+        if ( $PsBoundParameters.ContainsKey('secondary_server') ) {
+            $_tacacs | add-member -name "secondary-server" -membertype NoteProperty -Value $secondary_server
+        }
+
+        if ( $PsBoundParameters.ContainsKey('secondary_key') ) {
+            if (("Desktop" -eq $PSVersionTable.PsEdition) -or ($null -eq $PSVersionTable.PsEdition)) {
+                $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secondary_key);
+                $secondary_key_secure = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr);
+                $_tacacs | add-member -name "secondary-key" -membertype NoteProperty -Value $secondary_key_secure
+            }
+            else {
+                $secondary_key_secure = ConvertFrom-SecureString -SecureString $secondary_key -AsPlainText
+                $_tacacs | add-member -name "secondary-key" -membertype NoteProperty -Value $secondary_key_secure
+            }
+        }
+
+        if ( $PsBoundParameters.ContainsKey('tertiary_server') ) {
+            $_tacacs | add-member -name "tertiary-server" -membertype NoteProperty -Value $tertiary_server
+        }
+
+        if ( $PsBoundParameters.ContainsKey('tertiary_key') ) {
+            if (("Desktop" -eq $PSVersionTable.PsEdition) -or ($null -eq $PSVersionTable.PsEdition)) {
+                $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($tertiary_key);
+                $tertiary_key_secure = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr);
+                $_tacacs | add-member -name "tertiary-key" -membertype NoteProperty -Value $tertiary_key_secure
+            }
+            else {
+                $tertiary_key_secure = ConvertFrom-SecureString -SecureString $tertiary_key -AsPlainText
+                $_tacacs | add-member -name "tertiary-key" -membertype NoteProperty -Value $tertiary_key_secure
+            }
+        }
+
+        if ( $PsBoundParameters.ContainsKey('port') ) {
+            $_tacacs | add-member -name "port" -membertype NoteProperty -Value $port
+        }
+
+        if ( $PsBoundParameters.ContainsKey('authen_type') ) {
+            $tacacs | add-member -name "authen-type" -membertype NoteProperty -Value $authen_type
+        }
+
+        if ( $PsBoundParameters.ContainsKey('authorization') ) {
+            $tacacs | add-member -name "authorization" -membertype NoteProperty -Value $authorization
+        }
+
+        if ( $PsBoundParameters.ContainsKey('data') ) {
+            $data.GetEnumerator() | ForEach-Object {
+                $_tacacs | Add-member -name $_.key -membertype NoteProperty -Value $_.value
+            }
+        }
+
+        if ($PSCmdlet.ShouldProcess($usertacacs.name, 'Configure User TACACS')) {
+            Invoke-FGTRestMethod -method "PUT" -body $_tacacs -uri $uri -connection $connection @invokeParams | out-Null
+
+            Get-FGTUserTACACS -connection $connection @invokeParams -name $usertacacs.name
+        }
+    }
+
+    End {
+    }
+}
