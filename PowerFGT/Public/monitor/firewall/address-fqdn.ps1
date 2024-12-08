5cd1f2da15937ddc6dee6a0a8e30bc4b185e3da6
Alexis La Goutte <alexis.lagoutte@gmail.com>
2024-12-08 21:04:35 +0100

    Address(FQDN): add Get-FGTMonitorFirewallAddressFQDN
    for get status and number of addrs

---
+++ PowerFGT/Public/monitor/firewall/address-fqdn.ps1
@@ -0,0 +1,57 @@
+#
+# Copyright 2022, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Get-FGTMonitorFirewallAddressFQDN {
+
+    <#
+        .SYNOPSIS
+        Get Monitor Firewall Address FQDN
+
+        .DESCRIPTION
+        Get Monitor Firewall Adresss FQDN (fqdn, addrs, addrs_count...)
+
+        .EXAMPLE
+        Get-FGTMonitorFirewallAddressFQDN
+
+        Get ALL Firewall Address FQDN
+
+        .EXAMPLE
+        Get-FGTMonitorFirewallAddressFQDN -vdom vdomX
+
+        Get Firewall Address FQDN of vdomX
+
+    #>
+
+    Param(
+        [Parameter (Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        $uri = 'api/v2/monitor/firewall/address-fqdns?'
+
+        $response = Invoke-FGTRestMethod -uri $uri -method 'GET' -connection $connection @invokeParams
+        $response.results
+    }
+
+    End {
+    }
+}
