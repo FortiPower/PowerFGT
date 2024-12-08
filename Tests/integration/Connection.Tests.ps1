039b15523daaefede20bc95e18b8f73d637700e6
Alexis La Goutte <alexis.lagoutte@gmail.com>
2024-12-08 21:14:37 +0100

    Address(FQDN): add Tests

--- Tests/integration/Connection.Tests.ps1
+++ Tests/integration/Connection.Tests.ps1
@@ -274,6 +274,9 @@ Describe "Connect to a FortiGate (using multi connection)" {
         It "Use Multi connection for call Get Webfilter Profile" {
             { Get-FGTWebfilterProfile -connection $fgt } | Should -Not -Throw
         }
+        It "Use Multi connection for call Get Monitor Firewall Address FQDN" {
+            { Get-FGTMonitorFirewallAddressFQDN -connection $fgt } | Should -Not -Throw
+        }
         It "Use Multi connection for call Get Monitor Firewall Policy" {
             { Get-FGTMonitorFirewallPolicy -connection $fgt } | Should -Not -Throw
         }
