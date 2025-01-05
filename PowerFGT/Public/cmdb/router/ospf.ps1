6d244160657b650325b643c2c800cde8e309c232
Alexis La Goutte <alexis.lagoutte@gmail.com>
2025-01-05 09:40:56 +0100

    OSPF(router): Add Set-FGTRouterOSPF for configure OSPF
    only router_id or use -data for extra value

--- PowerFGT/Public/cmdb/router/ospf.ps1
+++ PowerFGT/Public/cmdb/router/ospf.ps1
@@ -68,3 +68,74 @@ function Get-FGTRouterOSPF {
     End {
     }
 }
+
+function Set-FGTRouterOSPF {
+
+    <#
+        .SYNOPSIS
+        Configure Router OSPF Configuration
+
+        .DESCRIPTION
+        Configure OSPF configuration (router id...)
+
+        .EXAMPLE
+        Set-FGTRouterOSPF -router_id "192.0.2.1"
+
+        Set OSPF Router ID to 192.0.2.1
+
+        .EXAMPLE
+        $data = @{ "bfd" = "enable" }
+        PS C> Set-FGTRouterOSPF -data $data
+
+        Change bfd settings using -data parameter
+
+    #>
+
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'medium')]
+    Param(
+        [Parameter (Mandatory = $false)]
+        [int]$as,
+        [Parameter (Mandatory = $false)]
+        [string]$router_id,
+        [Parameter (Mandatory = $false)]
+        [hashtable]$data,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        $_ospf = new-Object -TypeName PSObject
+
+        if ( $PsBoundParameters.ContainsKey('router_id') ) {
+            $_ospf | add-member -name "router-id" -membertype NoteProperty -Value $router_id
+        }
+
+        if ( $PsBoundParameters.ContainsKey('data') ) {
+            $data.GetEnumerator() | ForEach-Object {
+                $_ospf | Add-member -name $_.key -membertype NoteProperty -Value $_.value
+            }
+        }
+
+        $uri = 'api/v2/cmdb/router/ospf'
+
+        if ($PSCmdlet.ShouldProcess("OSPF", 'Configure Router OSPF')) {
+            Invoke-FGTRestMethod -uri $uri -method 'PUT' -body $_ospf -connection $connection @invokeParams | Out-Null
+        }
+
+        Get-FGTRouterOSPF -connection $connection @invokeParams
+    }
+
+    End {
+    }
+}
