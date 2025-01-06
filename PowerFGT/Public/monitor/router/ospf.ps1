63ed9f8914b3f107e3469d5ce53658a2c0decec8
Alexis La Goutte <alexis.lagoutte@gmail.com>
2025-01-06 14:03:58 +0100

    ospf(monitor): Add Get-FGTMontorRouterOSPFNeighbors
    for get OSPF Neighbors (ip, route-id, state, priority

---
+++ PowerFGT/Public/monitor/router/ospf.ps1
@@ -0,0 +1,58 @@
+#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Get-FGTMonitorRouterOSPFNeighbors {
+
+    <#
+        .SYNOPSIS
+        Get Router OSPF Neighbors
+
+        .DESCRIPTION
+        List all active discovered OSPF neighbors (ip, route-id, state, priority....)
+
+        .EXAMPLE
+        Get-FGTMonitorRouterOSPFNeighbors
+
+        Get ALL Router OSPF Neighbors
+
+        .EXAMPLE
+        Get-FGTMonitorRouterOSPFNeighbors -vdom vdomX
+
+        Get Router OSPF Neighbors monitor of vdomX
+
+    #>
+
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "")]
+    Param(
+        [Parameter (Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        $uri = 'api/v2/monitor/router/ospf/neighbors'
+
+        $response = Invoke-FGTRestMethod -uri $uri -method 'GET' -connection $connection @invokeParams
+        $response.results
+    }
+
+    End {
+    }
+}
