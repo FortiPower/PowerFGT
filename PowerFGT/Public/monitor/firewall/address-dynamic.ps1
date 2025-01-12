a4465c305d8b8c18329454f33b98877bbfe34a6d
Alexis La Goutte <alexis.lagoutte@gmail.com>
2025-01-12 20:17:34 +0100

    address-dynamic(monitor): Add Get-FGTMonitorFirewallAddressDynamic
    for get addr

---
+++ PowerFGT/Public/monitor/firewall/address-dynamic.ps1
@@ -0,0 +1,68 @@
+#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+function Get-FGTMonitorFirewallAddressDynamic {
+
+    <#
+        .SYNOPSIS
+        Get Monitor Firewall Address Dynamic
+
+        .DESCRIPTION
+        Get Monitor Firewall Adresss Dynamic (addr, subtype...)
+
+        .EXAMPLE
+        Get-FGTMonitorFirewallAddressDynamic
+
+        Get ALL Firewall Address Dynamic
+
+        .EXAMPLE
+        Get-FGTMonitorFirewallAddressDynamic -dynamic mySDN
+
+        Get Firewall Address Dynamic of mySDN
+
+        .EXAMPLE
+        Get-FGTMonitorFirewallAddressDynamic -vdom vdomX
+
+        Get Firewall Address Dynamic of vdomX
+
+    #>
+
+    Param(
+        [Parameter (Mandatory = $false, Position = 1)]
+        [string]$dynamic,
+        [Parameter (Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        $uri = 'api/v2/monitor/firewall/address-dynamic?'
+
+        if ($dynamic) {
+            $uri += "mkey=$($dynamic)"
+        }
+
+        $response = Invoke-FGTRestMethod -uri $uri -method 'GET' -connection $connection @invokeParams
+        $response.results
+    }
+
+    End {
+    }
+}
