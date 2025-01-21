9658b90d0379df5a339244bd91bfbbc81e5e30ec
Alexis La Goutte <alexis.lagoutte@gmail.com>
2025-01-21 08:05:26 +0100

    Wireless: Add Get-FGTWirelessGlobal

---
+++ PowerFGT/Public/cmdb/wireless/global.ps1
@@ -0,0 +1,88 @@
+﻿#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Get-FGTWirelessGlobal {
+
+    <#
+        .SYNOPSIS
+        Get list of all Wireless Global
+
+        .DESCRIPTION
+        Get list of all Wireless Global (Source, Destination, Protocol, Action...)
+
+        .EXAMPLE
+        Get-FGTWirelessGlobal
+
+        Get list of all Wireless Global object
+
+        .EXAMPLE
+        Get-FGTWirelessGlobal -meta
+
+        Get list of all Wireless Global object with metadata (q_...) like usage (q_ref)
+
+        .EXAMPLE
+        Get-FGTWirelessGlobal -skip
+
+        Get list of all Wireless Global object (but only relevant attributes)
+
+        .EXAMPLE
+        Get-FGTWirelessGlobal -vdom vdomX
+
+        Get list of all Wireless Global object on vdomX
+    #>
+
+    [CmdletBinding(DefaultParameterSetName = "default")]
+    Param(
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [string]$filter_attribute,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [ValidateSet('equal', 'contains')]
+        [string]$filter_type = "equal",
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [psobject]$filter_value,
+        [Parameter(Mandatory = $false)]
+        [switch]$meta,
+        [Parameter(Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('meta') ) {
+            $invokeParams.add( 'meta', $meta )
+        }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        #Filtering
+        #if filter value and filter_attribute, add filter (by default filter_type is equal)
+        if ( $filter_value -and $filter_attribute ) {
+            $invokeParams.add( 'filter_value', $filter_value )
+            $invokeParams.add( 'filter_attribute', $filter_attribute )
+            $invokeParams.add( 'filter_type', $filter_type )
+        }
+
+        $response = Invoke-FGTRestMethod -uri 'api/v2/cmdb/wireless-controller/global' -method 'GET' -connection $connection @invokeParams
+        $response.results
+    }
+
+    End {
+    }
+}
