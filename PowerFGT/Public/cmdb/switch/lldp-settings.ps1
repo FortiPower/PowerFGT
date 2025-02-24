ece03a2a563b740574db5ae9cc078a14c47181f9
Alexis La Goutte <alexis.lagoutte@gmail.com>
2025-02-24 08:18:04 +0100

    Switch: Add Get (Switch) LLDP Settings with Tests

---
+++ PowerFGT/Public/cmdb/switch/lldp-settings.ps1
@@ -0,0 +1,89 @@
+﻿#
+# Copyright 2019, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function Get-FGTSwitchLLDPSettings {
+
+    <#
+        .SYNOPSIS
+        Get list of Switch LLDP Settings
+
+        .DESCRIPTION
+        Get list of Switch LLDP Settings (tx, mangement interface, device ...)
+
+        .EXAMPLE
+        Get-FGTSwitchLLDPSettings
+
+        Get list of Switch LLDP Settings object
+
+        .EXAMPLE
+        Get-FGTSwitchLLDPSettings -meta
+
+        Get list of Switch LLDP Settings object with metadata (q_...) like usage (q_ref)
+
+        .EXAMPLE
+        Get-FGTSwitchLLDPSettings -skip
+
+        Get list of Switch LLDP Settings object (but only relevant attributes)
+
+        .EXAMPLE
+        Get-FGTSwitchLLDPSettings -vdom vdomX
+
+        Get list of Switch LLDP Settings object on vdomX
+    #>
+
+    [CmdletBinding(DefaultParameterSetName = "default")]
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "")]
+    Param(
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [string]$filter_attribute,
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [ValidateSet('equal', 'contains')]
+        [string]$filter_type = "equal",
+        [Parameter (Mandatory = $false)]
+        [Parameter (ParameterSetName = "filter")]
+        [psobject]$filter_value,
+        [Parameter(Mandatory = $false)]
+        [switch]$meta,
+        [Parameter(Mandatory = $false)]
+        [switch]$skip,
+        [Parameter(Mandatory = $false)]
+        [String[]]$vdom,
+        [Parameter(Mandatory = $false)]
+        [psobject]$connection = $DefaultFGTConnection
+    )
+
+    Begin {
+    }
+
+    Process {
+
+        $invokeParams = @{ }
+        if ( $PsBoundParameters.ContainsKey('meta') ) {
+            $invokeParams.add( 'meta', $meta )
+        }
+        if ( $PsBoundParameters.ContainsKey('skip') ) {
+            $invokeParams.add( 'skip', $skip )
+        }
+        if ( $PsBoundParameters.ContainsKey('vdom') ) {
+            $invokeParams.add( 'vdom', $vdom )
+        }
+
+        #Filtering
+        #if filter value and filter_attribute, add filter (by default filter_type is equal)
+        if ( $filter_value -and $filter_attribute ) {
+            $invokeParams.add( 'filter_value', $filter_value )
+            $invokeParams.add( 'filter_attribute', $filter_attribute )
+            $invokeParams.add( 'filter_type', $filter_type )
+        }
+
+        $response = Invoke-FGTRestMethod -uri 'api/v2/cmdb/switch-controller/lldp-settings' -method 'GET' -connection $connection @invokeParams
+        $response.results
+    }
+
+    End {
+    }
+}
