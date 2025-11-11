43d8d3973c4966f88f462a61f6043c93f22a85c6
Alexis La Goutte <alexis.lagoutte@gmail.com>
2025-11-11 21:10:08 +0100

    policy(firewall): remove extra space on .EXAMPLE about set data

--- PowerFGT/Public/cmdb/firewall/policy.ps1
+++ PowerFGT/Public/cmdb/firewall/policy.ps1
@@ -739,7 +739,7 @@ function Set-FGTFirewallPolicy {
         Change MyFGTPolicy to set Global Label "My Sequence Label"
 
         .EXAMPLE
-         $data = @{"logtraffic-start"  = "enable" }
+        $data = @{"logtraffic-start"  = "enable" }
         PS C:\>$MyFGTPolicy = Get-FGTFirewallPolicy -name MyFGTPolicy
         PS C:\>$MyFGTPolicy | Set-FGTFirewallPolicy -data $color
 
